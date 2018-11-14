988d10e76e1648d66f13b35b240721520a1ba02d
HiroshimaARC <hiroshima.arc@gmail.com>
2018-11-14 06:12:47 +0000

    アサートファースト

--- dev/20181114/k2works/main_test.py
+++ dev/20181114/k2works/main_test.py
@@ -1,7 +1,9 @@
 import unittest
 
 class MainTest(unittest.TestCase):
-    pass
+    
+    def test_1から100までの数をプリントする(self):
+        self.assertTrue(False)
 
 if __name__ == "__main__":
     unittest.main()
