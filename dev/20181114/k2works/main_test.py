5176c6b8c29e699603c646782594531dc6b6a364
HiroshimaARC <hiroshima.arc@gmail.com>
2018-11-14 06:45:13 +0000

    明白な実装

--- dev/20181114/k2works/main_test.py
+++ dev/20181114/k2works/main_test.py
@@ -5,7 +5,15 @@ def execute():
     n = 100
     
     while n != 0:
-        print(n)
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")        
+        elif n % 5 == 0:
+            print("Buzz")
+        else:
+            print(n)
+
         n = n - 1
 
 class MainTest(unittest.TestCase):
@@ -16,7 +24,30 @@ class MainTest(unittest.TestCase):
             lines = stdout.getvalue().splitlines()
             
         self.assertEqual(lines[99],"1")
-        self.assertEqual(lines[0],"100")
+        self.assertEqual(lines[0],"Buzz")
+        
+    def test_3の倍数のときは数の代わりにFizzをプリントする(self):
+        with captured_stdout() as stdout:
+            execute()
+            lines = stdout.getvalue().splitlines()
+            
+        self.assertEqual(lines[97],"Fizz") 
+        
+    def test_5の倍数のときはBuzzをプリントする(self):
+        with captured_stdout() as stdout:
+            execute()
+            lines = stdout.getvalue().splitlines()
+            
+        self.assertEqual(lines[95],"Buzz") 
+        
+    def test_3と5両方の倍数の場合にはFizzBuzzとプリントする(self):
+        with captured_stdout() as stdout:
+            execute()
+            lines = stdout.getvalue().splitlines()
+            
+        self.assertEqual(lines[85],"FizzBuzz")         
+        
+        
 
 if __name__ == "__main__":
     unittest.main()
