0c27530eaa1d1e9a981cbbec653812011cfb6f81
HiroshimaARC <hiroshima.arc@gmail.com>
2018-11-14 06:32:49 +0000

    三角測量

--- dev/20181114/k2works/main_test.py
+++ dev/20181114/k2works/main_test.py
@@ -2,15 +2,21 @@ import unittest
 from test.support import captured_stdout
 
 def execute():
-    print(1)
+    n = 100
+    
+    while n != 0:
+        print(n)
+        n = n - 1
 
 class MainTest(unittest.TestCase):
     
     def test_1から100までの数をプリントする(self):
         with captured_stdout() as stdout:
             execute()
+            lines = stdout.getvalue().splitlines()
             
-        self.assertEqual( stdout.getvalue(),"1\n")
+        self.assertEqual(lines[99],"1")
+        self.assertEqual(lines[0],"100")
 
 if __name__ == "__main__":
     unittest.main()
