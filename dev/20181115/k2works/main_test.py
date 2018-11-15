93501faa124f7a53860fb98a84825480e4078340
k2works <kakimomokuri@gmail.com>
2018-11-15 09:38:49 +0900

    明白な実装

--- dev/20181115/k2works/main_test.py
+++ dev/20181115/k2works/main_test.py
@@ -5,7 +5,10 @@ from test.support import captured_stdout
 def execute():
     n = 100
     while n != 0:
-        print(n)
+        if n % 3 == 0:
+            print("Fizz")
+        else:
+            print(n)
         n = n - 1
 
 
@@ -18,6 +21,13 @@ class MainTest(unittest.TestCase):
 
         self.assertEqual("100", lines[0])
         self.assertEqual("1", lines[99])
+
+    def test_3の倍数のときは数の代わりにFizzをプリントする(self):
+        with captured_stdout() as stdout:
+            execute()
+            lines = stdout.getvalue().splitlines()
+
+        self.assertEqual("Fizz", lines[97])
 
 
 if __name__ == "__main__":
