bf45f0820625dfe4be40c9d5c186af51b5c2b09d
k2works <kakimomokuri@gmail.com>
2018-11-16 19:59:13 +0900

    refactor:メソッドの抽出

--- dev/20181116/session/main_test.py
+++ dev/20181116/session/main_test.py
@@ -2,37 +2,42 @@ import unittest
 from test.support import captured_stdout
 
 def execute():
-    n = 100
-    while n != 0:
-        if n % 3 == 0 and n % 5 == 0:
-            print("FizzBuzz")        
-        elif n % 3 == 0:
-            print("Fizz")
-        elif n % 5 == 0:
-            print("Buzz")
-        else:            
-            print(n) 
-        n = n - 1       
+    iterate(101)
+
+def iterate(c):
+    for n in range(c):
+        generate(n)    
+
+def generate(n):
+    if n % 3 == 0 and n % 5 == 0:
+         print("FizzBuzz")        
+    elif n % 3 == 0:
+         print("Fizz")
+    elif n % 5 == 0:
+         print("Buzz")
+    else:            
+         print(n)         
+
 
 class MainTest(unittest.TestCase):
-    
+
     def setUp(self):
         with captured_stdout() as stdout:
             execute()
             self.lines = stdout.getvalue().splitlines()
     
     def test_1から100まで数をプリントできるようにする(self):        
-        self.assertEqual( "1", self.lines[99])
-        self.assertEqual("Buzz", self.lines[0])
+        self.assertEqual( "1", self.lines[1])
+        self.assertEqual("Buzz", self.lines[100])
 
     def test_3の倍数のときは数の代わりにFizzをプリントする(self):        
-        self.assertEqual("Fizz", self.lines[97])
+        self.assertEqual("Fizz", self.lines[3])
 
     def test_5の倍数のときはBuzzとプリントする(self):
-        self.assertEqual("Buzz", self.lines[95])        
+        self.assertEqual("Buzz", self.lines[5])        
 
     def test_3と5両方の倍数の場合にはFizzBuzzとプリントする(self):
-        self.assertEqual("FizzBuzz", self.lines[85])                
+        self.assertEqual("FizzBuzz", self.lines[15])                
 
 
 if __name__ == "__main__":
