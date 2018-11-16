c5d3b2714f3fc05c144f3c089d76baf51f51171b
k2works <kakimomokuri@gmail.com>
2018-11-16 19:15:38 +0900

    feat:明白な実装

--- dev/20181116/session/main_test.py
+++ dev/20181116/session/main_test.py
@@ -6,6 +6,8 @@ def execute():
     while n != 0:
         if n % 3 == 0:
             print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
         else:            
             print(n) 
         n = n - 1       
@@ -18,7 +20,7 @@ class MainTest(unittest.TestCase):
             lines = stdout.getvalue().splitlines()
 
         self.assertEqual( "1", lines[99])
-        self.assertEqual("100", lines[0])
+        self.assertEqual("Buzz", lines[0])
 
     def test_3の倍数のときは数の代わりにFizzをプリントする(self):
         with captured_stdout() as stdout:
@@ -26,6 +28,14 @@ class MainTest(unittest.TestCase):
             lines = stdout.getvalue().splitlines()
 
         self.assertEqual("Fizz", lines[97])
+
+    def test_5の倍数のときはBuzzとプリントする(self):
+        with captured_stdout() as stdout:
+            execute()
+            lines = stdout.getvalue().splitlines()
+
+        self.assertEqual("Buzz", lines[95])        
+
 
 
 
