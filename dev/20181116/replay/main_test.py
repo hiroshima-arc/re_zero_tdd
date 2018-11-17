7d52523f30f446fe0e2ff0c57d27f09d9e206f89
k2works <kakimomokuri@gmail.com>
2018-11-17 16:42:00 +0900

    docs: アサートファースト

--- dev/20181116/replay/main_test.py
+++ dev/20181116/replay/main_test.py
@@ -1,8 +1,13 @@
 import unittest
 
 
+def execute():
+    return "1"
+
+
 class MainTest(unittest.TestCase):
-    pass
+    def test_1から100まで数をプリントできるようにする(self):
+        self.assertEqual("1", execute())
 
 
 if __name__ == "__main__":
