aca2eb4bffd8cc632bb52aae41c1eaa60184f32b
k2works <kakimomokuri@gmail.com>
2018-11-18 11:50:58 +0900

    docs: 明白な実装

--- dev/20181116/replay/main_test.py
+++ dev/20181116/replay/main_test.py
@@ -5,7 +5,9 @@ from test.support import captured_stdout
 def execute():
     n = 100
     while n != 0:
-        if n % 3 == 0:
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
             print("Fizz")
         elif n % 5 == 0:
             print("Buzz")
@@ -34,6 +36,12 @@ class MainTest(unittest.TestCase):
             execute()
             lines = stdout.getvalue().splitlines()
         self.assertEqual("Buzz", lines[95])
+
+    def test_3と5両方の倍数の場合にはFizzBuzzとプリントする(self):
+        with captured_stdout() as stdout:
+            execute()
+            lines = stdout.getvalue().splitlines()
+        self.assertEqual("FizzBuzz", lines[85])
 
 
 if __name__ == "__main__":
