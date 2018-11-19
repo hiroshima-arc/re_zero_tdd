bc460bde79605c2fc8dad2537ba7cf6ead02c4f6
k2works <kakimomokuri@gmail.com>
2018-11-19 13:42:02 +0900

    docs: アルゴリズムの取り替え

--- dev/20181116/replay/main_test.py
+++ dev/20181116/replay/main_test.py
@@ -3,8 +3,7 @@ from test.support import captured_stdout
 
 
 def execute():
-    n = 1
-    while n != 101:
+    for n in range(101):
         if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz")
         elif n % 3 == 0:
@@ -13,39 +12,26 @@ def execute():
             print("Buzz")
         else:
             print(n)
-        n = n + 1
-
-
-def execute_for():
-    for n in range(101):
-        print(n)
 
 
 class MainTest(unittest.TestCase):
-    def test_execute_for(self):
-        with captured_stdout() as stdout:
-            execute_for()
-            lines = stdout.getvalue().splitlines()
-        self.assertEqual("1", lines[1])
-        self.assertEqual("100", lines[-1])
-
     def setUp(self):
         with captured_stdout() as stdout:
             execute()
             self.lines = stdout.getvalue().splitlines()
 
     def test_1から100まで数をプリントできるようにする(self):
-        self.assertEqual("1", self.lines[0])
-        self.assertEqual("Buzz", self.lines[99])
+        self.assertEqual("1", self.lines[1])
+        self.assertEqual("Buzz", self.lines[100])
 
     def test_3の倍数のときは数の代わりにFizzをプリントする(self):
-        self.assertEqual("Fizz", self.lines[2])
+        self.assertEqual("Fizz", self.lines[3])
 
     def test_5の倍数のときはBuzzとプリントする(self):
-        self.assertEqual("Buzz", self.lines[4])
+        self.assertEqual("Buzz", self.lines[5])
 
     def test_3と5両方の倍数の場合にはFizzBuzzとプリントする(self):
-        self.assertEqual("FizzBuzz", self.lines[14])
+        self.assertEqual("FizzBuzz", self.lines[15])
 
 
 class RangeTest(unittest.TestCase):
