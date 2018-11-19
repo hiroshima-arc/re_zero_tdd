4ac51d0e9f243f82ebc0901fd0c80058fa6a796d
k2works <kakimomokuri@gmail.com>
2018-11-19 10:53:57 +0900

    docs: ベイビーステップ

--- dev/20181116/replay/main_test.py
+++ dev/20181116/replay/main_test.py
@@ -3,8 +3,8 @@ from test.support import captured_stdout
 
 
 def execute():
-    n = 100
-    while n != 0:
+    n = 1
+    while n != 101:
         if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz")
         elif n % 3 == 0:
@@ -13,7 +13,7 @@ def execute():
             print("Buzz")
         else:
             print(n)
-        n = n - 1
+        n = n + 1
 
 
 class MainTest(unittest.TestCase):
@@ -23,17 +23,17 @@ class MainTest(unittest.TestCase):
             self.lines = stdout.getvalue().splitlines()
 
     def test_1から100まで数をプリントできるようにする(self):
-        self.assertEqual("1", self.lines[99])
-        self.assertEqual("Buzz", self.lines[0])
+        self.assertEqual("1", self.lines[0])
+        self.assertEqual("Buzz", self.lines[99])
 
     def test_3の倍数のときは数の代わりにFizzをプリントする(self):
-        self.assertEqual("Fizz", self.lines[97])
+        self.assertEqual("Fizz", self.lines[2])
 
     def test_5の倍数のときはBuzzとプリントする(self):
-        self.assertEqual("Buzz", self.lines[95])
+        self.assertEqual("Buzz", self.lines[4])
 
     def test_3と5両方の倍数の場合にはFizzBuzzとプリントする(self):
-        self.assertEqual("FizzBuzz", self.lines[85])
+        self.assertEqual("FizzBuzz", self.lines[14])
 
 
 if __name__ == "__main__":
