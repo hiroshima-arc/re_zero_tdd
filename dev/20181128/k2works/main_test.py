288d3e9bba1a41f9d4316698822f51dd5f11557a
k2works <kakimomokuri@gmail.com>
2018-11-28 15:09:27 +0900

    feat: fizz_buzz

--- dev/20181128/k2works/main_test.py
+++ dev/20181128/k2works/main_test.py
@@ -3,7 +3,13 @@ from test.support import captured_stdout
 
 def execute():
     for n in range(101):
-        print(n)
+        print(fizz_buzz(n))
+
+def fizz_buzz(n):
+    if n % 3 == 0:
+        return 'Fizz'
+    else:
+        return n
 
 class MainTest(unittest.TestCase):
     def testカウントが1から100までプリントする(self):
@@ -13,6 +19,9 @@ class MainTest(unittest.TestCase):
 
         self.assertEqual(lines[1], '1')
         self.assertEqual(lines[100], '100')
+
+    def test値が3ならばFizzをプリントする(self):
+        self.assertEqual('Fizz', fizz_buzz(3))
 
 if __name__ == "__main__":
     unittest.main()
