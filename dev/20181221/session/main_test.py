825355e919af1a1d3a3a611dfaf42d6d2afeb248
k2works <kakimomokuri@gmail.com>
2018-12-21 19:55:37 +0900

    test: 3で割り切れたらFizzを返す

--- dev/20181221/session/main_test.py
+++ dev/20181221/session/main_test.py
@@ -7,12 +7,19 @@ class TestMain(unittest.TestCase):
       self.assertEqual(1 , actual[1])
       self.assertEqual(100, actual[-1])
 
+    def test_3で割り切れたらFizzを返す(self):
+        self.assertEqual('Fizz',executeFizz(3))
+
 def print1from100():
     array = []
     for i in range(101):
         array.append(i)
 
     return array
+
+def executeFizz(num):
+    if (num % 3) == 0:
+        return 'Fizz'
 
 if __name__ == '__main__':
     unittest.main()
