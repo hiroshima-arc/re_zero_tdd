889f003e8afac6473591a23c4b7a84ce8f60251d
k2works <kakimomokuri@gmail.com>
2018-12-21 19:45:28 +0900

    feat: 1から100までを出力する

--- dev/20181221/session/main_test.py
+++ dev/20181221/session/main_test.py
@@ -2,7 +2,17 @@ import unittest
 
 class TestMain(unittest.TestCase):
     def test1から100までを出力する(self):
-      self.assertEqual('1' ,1 + 1)
+      actual = []
+      actual = print1from100()
+      self.assertEqual(1 , actual[1])
+      self.assertEqual(100, actual[-1])
+
+def print1from100():
+    array = []
+    for i in range(101):
+        array.append(i)
+
+    return array
 
 if __name__ == '__main__':
     unittest.main()
