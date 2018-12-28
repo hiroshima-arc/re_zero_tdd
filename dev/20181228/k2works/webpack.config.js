0a294571e800a1003953799a6077329a5925be07
k2works <kakimomokuri@gmail.com>
2018-12-28 17:41:28 +0900

    chore: npmパッケージセットアップ

---
+++ dev/20181228/k2works/webpack.config.js
@@ -0,0 +1,37 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  devServer: {
+    contentBase: "dist",
+    open: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env"
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
