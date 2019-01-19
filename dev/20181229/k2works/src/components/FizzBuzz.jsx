0e5c297fd425ededa6db3f039256529daf040103
k2works <kakimomokuri@gmail.com>
2019-01-19 19:13:48 +0900

    refactor: メソッドの移動

--- dev/20181229/k2works/src/components/FizzBuzz.jsx
+++ dev/20181229/k2works/src/components/FizzBuzz.jsx
@@ -1,7 +1,199 @@
 import React from "react";
 import styles from "./FizzBuzz.css";
+import $ from "jquery";
+import * as constants from "../constants";
+import {fizzBuzz} from "../fizzbuzz_model";
+import {iterate} from "../fizzbuzz_model";
+
+const MAX_COUNT = constants.FIZZ_BUZZ_MAX_COUNT;
 
 export class FizzBuzz extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoad = this.handleLoad.bind(this);
+    this.showFizzBuzzCall = this.showFizzBuzzCall.bind(this);
+    this.showFizzBuzzPrint = this.showFizzBuzzPrint.bind(this);
+    this.showFizzBuzzUpDown = this.showFizzBuzzUpDown.bind(this);
+    this.showFizzBuzzIterate = this.showFizzBuzzIterate.bind(this);
+    this.fizzBuzzIterate = this.fizzBuzzIterate.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("load", this.handleLoad);
+  }
+
+  handleLoad() {
+    [{
+      "nav-component__sub-menu--fizz-buzz-call": this.showFizzBuzzCall
+    },
+      {
+        "nav-component__sub-menu--fizz-buzz-print": this.showFizzBuzzPrint
+      },
+      {
+        "nav-component__sub-menu--fizz-buzz-up-down": this.showFizzBuzzUpDown
+      },
+      {
+        "nav-component__sub-menu--fizz-buzz-iterate": this.showFizzBuzzIterate
+      },
+      {
+        "fizz-buzz-component__button--call": this.fizzBuzzCall
+      },
+      {
+        "fizz-buzz-component__button--print": this.fizzBuzzPrint
+      },
+      {
+        "fizz-buzz-component__button--down": this.fizzBuzzDown
+      },
+      {
+        "fizz-buzz-component__button--up": this.fizzBuzzUp
+      },
+      {
+        "fizz-buzz-component__button--iterate": this.fizzBuzzIterate
+      }
+    ].forEach(hash => {
+      Object.keys(hash).forEach(key => {
+        let menu = $(`#${key}`);
+        menu.on("click", hash[key]);
+      });
+    });
+  }
+
+  showFizzBuzzCall() {
+    this.showFizzBuzz({
+      "call": "block",
+      "print": "none",
+      "updown": "none",
+      "iterate": "none"
+    });
+  }
+
+  showFizzBuzzPrint() {
+    this.showFizzBuzz({
+      "call": "none",
+      "print": "block",
+      "updown": "none",
+      "iterate": "none"
+    });
+  }
+
+  showFizzBuzzUpDown() {
+    this.showFizzBuzz({
+      "call": "none",
+      "print": "none",
+      "updown": "block",
+      "iterate": "none"
+    });
+  }
+
+  showFizzBuzzIterate() {
+    this.showFizzBuzz({
+      "call": "none",
+      "print": "none",
+      "updown": "none",
+      "iterate": "block"
+    });
+  }
+
+  showFizzBuzz(
+    state = {
+      "call": "none",
+      "print": "none",
+      "updown": "none",
+      "iterate": "none"
+    }
+  ) {
+    $("#fizz-buzz-component__article--call").css("display", state["call"]);
+    $("#fizz-buzz-component__article--print").css("display", state["print"]);
+    $("#fizz-buzz-component__article--up-down").css("display", state["updown"]);
+    $("#fizz-buzz-component__article--iterate").css("display", state["iterate"]);
+  }
+
+  fizzBuzzCall() {
+    const value = $("#fizz-buzz-component__input--call").val();
+    if (isNaN(value)) {
+      alert("数字を入力してください。");
+    } else {
+      alert(fizzBuzz(value));
+    }
+  }
+
+  fizzBuzzPrint() {
+    const value = $("#fizz-buzz-component__input--print").val();
+    const target = $("#fizz-buzz-component__print--message");
+    if (isNaN(value)) {
+      a;
+      target.html("数字を入力してください。");
+    } else {
+      target.html(fizzBuzz(value));
+    }
+  }
+
+  fizzBuzzDown() {
+    if (fizzBuzzUpDownCount === 0) {
+      fizzBuzzUpDownCount = 0;
+    } else {
+      fizzBuzzUpDownCount -= 1;
+    }
+    const target = $("#fizz-buzz-component__up-down--message");
+    const count = fizzBuzzUpDownCount;
+    target.html(fizzBuzz(count));
+  }
+
+  fizzBuzzUp() {
+    fizzBuzzUpDownCount += 1;
+    const target = $("#fizz-buzz-component__up-down--message");
+    const count = fizzBuzzUpDownCount;
+    const value = fizzBuzz(count);
+    target.html(value);
+  }
+
+  fizzBuzzIterate() {
+    const target = $("#fizz-buzz-component__iterate--result");
+    let count = $("#fizz-buzz-component__input--iterate").val();
+    if (isNaN(count)) {
+      return target.html("数字を入力してください。");
+    }
+    if (count > MAX_COUNT) {
+      return target.html(`<strong>件数は${MAX_COUNT}までです。</strong>`);
+    }
+    let array = iterate(parseInt(count));
+    target.html(this.renderHtmlTable(array));
+  }
+
+  renderHtmlTable(array) {
+    let rowCount = 1;
+    let html = "<table>";
+
+    html = html + "<thead>";
+    [...Array(10).keys()].forEach((v) => {
+      html = html + `<th>${v + 1}</th>`;
+    });
+    html = html + "</thead>";
+
+    html = html + "<tbody>";
+
+    html = html + "<tr>";
+    array.forEach((value) => {
+      if (rowCount > 10) {
+        html = html + "<tr>";
+      }
+
+      value = `<td>${value}</td>`;
+      html = html + value;
+
+      if (rowCount === 10) {
+        html = html + "</tr>";
+        rowCount = 1;
+      } else {
+        rowCount += 1;
+      }
+    });
+
+    html = html + "</tbody>";
+    html = html + "</table>";
+    return html;
+  }
+
   render() {
     return (
       <div>
@@ -11,7 +203,7 @@ export class FizzBuzz extends React.Component {
             <nav>
               <h1>Menu</h1>
               <ul>
-                <a href="../index.html">FizzBuzz</a>
+                <a href="#">FizzBuzz</a>
                 <ul id="nav-component__sub-menu--fizz-buzz">
                   <li id="nav-component__sub-menu--fizz-buzz-call">
                     <a href="#">FizzBuzzCall</a>
