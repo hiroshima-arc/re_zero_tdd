cdc264bbfbf6f1f94bc1d77c6b911f244ed55672
k2works <kakimomokuri@gmail.com>
2019-02-16 13:35:57 +0900

    test: 1から100までの数をプリントする

--- dev/20190216/k2works/fizz_buzz.py
+++ dev/20190216/k2works/fizz_buzz.py
@@ -1,13 +1,30 @@
 import unittest
 
+
 class TestFizzBuzz(unittest.TestCase):
-  def testGreeting(self):
-    self.assertEqual('Hello, World!', FizzBuzz.greeting())
+    def testGreeting(self):
+        self.assertEqual('Hello, World!', FizzBuzz.greeting())
+
+    def test_1から100までの数をプリントする(self):
+        array = FizzBuzz.generate_array()
+        self.assertEqual(1, array[0])
+        self.assertEqual(100, array[-1])
+
 
 class FizzBuzz:
-  @staticmethod
-  def greeting():
-    return 'Hello, World!'
+    @staticmethod
+    def greeting():
+        return 'Hello, World!'
+
+    @staticmethod
+    def generate_array():
+        array = []
+
+        for i in range(100):
+            array.append(i + 1)
+
+        return array
+
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
