33817b8fbaed4f88d118997f68e21e83445a2531
k2works <kakimomokuri@gmail.com>
2019-02-18 10:43:30 +0900

    test: 1から100までの数をプリントする

--- dev/20190218/k2works/fizz_buzz.rb
+++ dev/20190218/k2works/fizz_buzz.rb
@@ -4,10 +4,27 @@ class TestFizzBuzz < Test::Unit::TestCase
   test 'greeting' do
     assert_equal 'Hello, world!', FizzBuzz.greeting
   end
+
+  test '1から100までの数をプリントする' do
+    list = FizzBuzz.print
+    assert_equal 1, list.first
+    assert_equal 100, list.last
+  end
 end
 
 class FizzBuzz
   def self.greeting
     'Hello, world!'
+  end
+
+  def self.print
+    list = []
+    i = 1
+    while i <= 100
+      list.push(i)
+      i += 1
+    end
+    puts list
+    list
   end
 end
