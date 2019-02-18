081fba4d09cba83df390a5f95301a066e29769e4
k2works <kakimomokuri@gmail.com>
2019-02-18 11:10:52 +0900

    test: 1から100までの数をプリントする

--- dev/20190218/k2works/fizzBuzz.js
+++ dev/20190218/k2works/fizzBuzz.js
@@ -1,17 +1,42 @@
 const assert = chai.assert;
 suite("FizzBuzzTest", () => {
-  test("greeting", () => {
-    assert.equal("Hello, World!", FizzBuzz.greeting());
+  test("1から100までの数をプリントする", () => {
+    const list = FizzBuzz.print();
+    assert.equal(1, list[0]);
   });
 });
 
 const FizzBuzz = {
-  greeting: function() {
-    return "Hello, World!";
-  },
-
   print() {
+    let list = [];
+    for (let i = 1; i <= 100; i++) {
+      list.push(i);
+    }
+
+    let columnCount = 1;
+    let html = "<table>";
+    html += "<thead>";
+    [...Array(10).keys()].forEach((key) => {
+      html += `<th>${key + 1}</th>`;
+    });
+    html += "</thead>";
+    html += "<tbody>";
+    html += "<tr>";
+    list.forEach((v) => {
+      if (columnCount < 10) {
+        html += `<td>${v}</td>`;
+        columnCount += 1;
+      } else {
+        html += `<td>${v}</td>`;
+        html += "</tr>";
+        columnCount = 1;
+      }
+    });
+    html += "</tbody>";
+    html += "</table>";
     const app = document.querySelector("#app");
-    app.innerHTML = this.greeting();
+    app.innerHTML = html;
+
+    return list;
   }
 };
