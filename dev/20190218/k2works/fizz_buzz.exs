03326c3f23411ff39542522b6a49d910efbf573a
k2works <kakimomokuri@gmail.com>
2019-02-18 12:29:47 +0900

    test: 3で割り切れる場合はFizzを返す

--- dev/20190218/k2works/fizz_buzz.exs
+++ dev/20190218/k2works/fizz_buzz.exs
@@ -8,6 +8,11 @@ defmodule FizzBuzzTest do
     assert hd(list) == 1
     assert List.last(list) == 100
   end
+
+  test "3で割り切れる場合はFizzを返す" do
+    assert FizzBuzz.exec(3) == "Fizz"
+    assert FizzBuzz.exec(1) == 1
+  end
 end
 
 defmodule FizzBuzz do
@@ -22,5 +27,13 @@ defmodule FizzBuzz do
   defp _print(current, left, result) do
     IO.puts(current)
     _print(current+1, left-1, [ current | result])
+  end
+
+  def exec(number) do
+    if rem(number,3) == 0 do
+      "Fizz"
+    else
+      number
+    end
   end
 end
