a5b34f6d83784d6a6702e0d88bb9d7df1b2b59b3
k2works <kakimomokuri@gmail.com>
2019-02-18 11:27:45 +0900

    test: 1から100までの数をプリントする

--- dev/20190218/k2works/fizz_buzz.exs
+++ dev/20190218/k2works/fizz_buzz.exs
@@ -3,13 +3,24 @@ ExUnit.start()
 defmodule FizzBuzzTest do
   use ExUnit.Case, async: true
 
-  test "Hello, world!" do
-    assert FizzBuzz.greeting() == "Hello, world!"
+  test "1から100までの数をプリントする" do
+    list = FizzBuzz.print(100)
+    assert hd(list) == 1
+    assert List.last(list) == 100
   end
 end
 
 defmodule FizzBuzz do
-  def greeting do
-    "Hello, world!"
+  def print(n) when n > 0 do
+    _print(1, n , [])
+  end
+
+  defp _print(_current, 0, result) do
+    Enum.reverse(result)
+  end
+
+  defp _print(current, left, result) do
+    IO.puts(current)
+    _print(current+1, left-1, [ current | result])
   end
 end
