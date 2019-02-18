d1e42e98a988a000e992809300741f7d5ab3e52e
k2works <kakimomokuri@gmail.com>
2019-02-18 13:46:38 +0900

    feat: FizzBuzz

--- dev/20190218/k2works/fizz_buzz.exs
+++ dev/20190218/k2works/fizz_buzz.exs
@@ -3,10 +3,13 @@ ExUnit.start()
 defmodule FizzBuzzTest do
   use ExUnit.Case, async: true
 
-  test "1から100までの数をプリントする" do
+  test "1から100までの数をプリントする,ただし3で割り切れる場合はFizz,5で割り切れる場合はBuzz,両方で割り切れる場合はFizzBuzzをプリントする" do
     list = FizzBuzz.print(100)
-    assert hd(list) == 1
-    assert List.last(list) == 100
+    assert List.first(list) == 1
+    assert Enum.take(list, 3)|>List.last == "Fizz"
+    assert Enum.take(list, 5)|>List.last == "Buzz"
+    assert Enum.take(list, 15)|>List.last == "FizzBuzz"
+    assert List.last(list) == "Buzz"
   end
 
   test "3で割り切れる場合はFizzを返す" do
@@ -25,16 +28,17 @@ end
 
 defmodule FizzBuzz do
   def print(n) when n > 0 do
-    _print(1, n , [])
+    _print(n , [])
   end
 
-  defp _print(_current, 0, result) do
-    Enum.reverse(result)
+  defp _print(0, result) do
+    result
   end
 
-  defp _print(current, left, result) do
-    IO.puts(current)
-    _print(current+1, left-1, [ current | result])
+  defp _print(current, result) do
+    next_answer = exec(current)
+    IO.puts(next_answer)
+    _print(current-1, [ next_answer | result])
   end
 
   def exec(number) when rem(number,3) == 0 and rem(number,5) == 0 do
