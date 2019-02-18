f133a5569c6b62bd6bd3fcfffcd179257977da94
k2works <kakimomokuri@gmail.com>
2019-02-18 09:36:36 +0900

    test: HelloWorld

---
+++ dev/20190218/k2works/fizz_buzz.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+class TestFizzBuzz < Test::Unit::TestCase
+  test 'greeting' do
+    assert_equal 'Hello, world!', FizzBuzz.greeting
+  end
+end
+
+class FizzBuzz
+  def self.greeting
+    'Hello, world!'
+  end
+end
