05dc389be9d36836c18271c1d181a36477c27c1e
k2works <kakimomokuri@gmail.com>
2019-02-19 14:49:26 +0900

    style: フォーマット

--- dev/20190218/k2works/fizz_buzz.exs
+++ dev/20190218/k2works/fizz_buzz.exs
@@ -6,9 +6,9 @@ defmodule FizzBuzzTest do
   test "1から100までの数をプリントする,ただし3で割り切れる場合はFizz,5で割り切れる場合はBuzz,両方で割り切れる場合はFizzBuzzをプリントする" do
     list = FizzBuzz.print(100)
     assert List.first(list) == 1
-    assert Enum.take(list, 3)|>List.last == "Fizz"
-    assert Enum.take(list, 5)|>List.last == "Buzz"
-    assert Enum.take(list, 15)|>List.last == "FizzBuzz"
+    assert Enum.take(list, 3) |> List.last() == "Fizz"
+    assert Enum.take(list, 5) |> List.last() == "Buzz"
+    assert Enum.take(list, 15) |> List.last() == "FizzBuzz"
     assert List.last(list) == "Buzz"
   end
 
@@ -27,33 +27,21 @@ defmodule FizzBuzzTest do
 end
 
 defmodule FizzBuzz do
-  def print(n) when n > 0 do
-    _print(n , [])
-  end
+  def print(n) when n > 0, do: _print(n, [])
 
-  defp _print(0, result) do
-    result
-  end
+  defp _print(0, result), do: result
 
   defp _print(current, result) do
     next_answer = exec(current)
     IO.puts(next_answer)
-    _print(current-1, [ next_answer | result])
+    _print(current - 1, [next_answer | result])
   end
 
-  def exec(number) when rem(number,3) == 0 and rem(number,5) == 0 do
-    "FizzBuzz"
-  end
+  def exec(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: "FizzBuzz"
 
-  def exec(number) when rem(number,3) == 0 do
-    "Fizz"
-  end
+  def exec(number) when rem(number, 3) == 0, do: "Fizz"
 
-  def exec(number) when rem(number,5) == 0 do
-    "Buzz"
-  end
+  def exec(number) when rem(number, 5) == 0, do: "Buzz"
 
-  def exec(number) do
-    number
-  end
+  def exec(number), do: number
 end
