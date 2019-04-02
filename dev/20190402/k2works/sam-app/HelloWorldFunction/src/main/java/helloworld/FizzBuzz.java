2f64ae5e94a1484e9c745db837ecc067208f88b5
k2works <kakimomokuri@gmail.com>
2019-04-02 13:31:27 +0900

    refactor: ループからコレクションクロージャメソッドへ

--- dev/20190402/k2works/sam-app/HelloWorldFunction/src/main/java/helloworld/FizzBuzz.java
+++ dev/20190402/k2works/sam-app/HelloWorldFunction/src/main/java/helloworld/FizzBuzz.java
@@ -1,24 +1,17 @@
 package helloworld;
 
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public static List<String> generateFizzBuzzList() {
-        List<String> list = new ArrayList<>();
-        IntStream.rangeClosed(1,100).forEach(i -> {
-            String result = FizzBuzz.fizzBuzz(i);
-            list.add(result);
-        });
-        return list;
+        return IntStream.rangeClosed(1,100).mapToObj(FizzBuzz::fizzBuzz).collect(Collectors.toList());
     }
 
     public static void print(List<String> list) {
-        for(String s : list) {
-            System.out.println(s);
-        }
+        list.forEach(System.out::println);
     }
 
     public static String fizzBuzz(int i) {
