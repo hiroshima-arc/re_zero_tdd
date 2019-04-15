b22da7635e8996289f67d02c8fb80959be42bd29
k2works <kakimomokuri@gmail.com>
2019-04-15 12:20:46 +0900

    feat: Webインターフェースセットアップ

---
+++ dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/controller/FizzBuzzController.java
@@ -0,0 +1,15 @@
+package org.hiroshima_arc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FizzBuzzController {
+    @RequestMapping(value = "/fizzbuzz", method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView mav) {
+        mav.setViewName("index");
+        return mav;
+    }
+}
