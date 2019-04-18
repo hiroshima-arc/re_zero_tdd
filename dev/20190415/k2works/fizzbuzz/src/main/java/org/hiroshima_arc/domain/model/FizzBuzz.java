62bbb6c17cec0ff735f7821ff092a052d78ddc8b
k2works <kakimomokuri@gmail.com>
2019-04-18 12:20:48 +0900

    refactor: 一時変数の導入

--- dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/domain/model/FizzBuzz.java
+++ dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/domain/model/FizzBuzz.java
@@ -14,9 +14,11 @@ public class FizzBuzz {
     }
 
     public static String generate(int number) {
-        if (number % 3 == 0 && number % 5 == 0) return  "FizzBuzz";
-        if (number % 3 == 0) return  "Fizz";
-        if (number % 5 == 0) return "Buzz";
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+        if (isFizz && isBuzz) return  "FizzBuzz";
+        if (isFizz) return  "Fizz";
+        if (isBuzz) return "Buzz";
         return Integer.toString(number);
     }
 }
