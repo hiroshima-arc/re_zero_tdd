5f65d59850d89d28a5988069cdfa37df606be5a5
k2works <kakimomokuri@gmail.com>
2019-04-18 12:17:21 +0900

    refactor: 条件分岐のネストからガード節へ

--- dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/domain/model/FizzBuzz.java
+++ dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/domain/model/FizzBuzz.java
@@ -14,14 +14,9 @@ public class FizzBuzz {
     }
 
     public static String generate(int number) {
-        String result = Integer.toString(number);
-        if (number % 3 == 0 && number % 5 == 0) {
-            result = "FizzBuzz";
-        }else if (number % 3 == 0) {
-            result = "Fizz";
-        } else if (number % 5 == 0) {
-            result = "Buzz";
-        }
-        return result;
+        if (number % 3 == 0 && number % 5 == 0) return  "FizzBuzz";
+        if (number % 3 == 0) return  "Fizz";
+        if (number % 5 == 0) return "Buzz";
+        return Integer.toString(number);
     }
 }
