89a4b4ccb11a47c2897908b07937f0eafd7fd4fc
k2works <kakimomokuri@gmail.com>
2019-04-19 09:21:37 +0900

    refactor: レコードからデータクラスへ

---
+++ dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/domain/model/FizzBuzzData.java
@@ -0,0 +1,24 @@
+package org.hiroshima_arc.domain.model;
+
+import java.util.List;
+
+public class FizzBuzzData {
+    private String _value;
+    private List<String> _values;
+
+    public String getValue() {
+        return _value;
+    }
+
+    public void setValue(String _value) {
+        this._value = _value;
+    }
+
+    public List<String> getValues() {
+        return _values;
+    }
+
+    public void setValues(List<String> _values) {
+        this._values = _values;
+    }
+}
--- dev/20190415/k2works/fizzbuzz/src/test/java/org/hiroshima_arc/domain/model/FizzBuzzTest.java
+++ dev/20190415/k2works/fizzbuzz/src/test/java/org/hiroshima_arc/domain/model/FizzBuzzTest.java
@@ -1,36 +1,44 @@
 package org.hiroshima_arc.domain.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    private FizzBuzzData _fizzBuzzData;
+    @BeforeEach
+    void init() {
+       _fizzBuzzData = new FizzBuzzData();
+    }
+
     @Test
     @DisplayName("値を1から100までプリントする,ただし3で割り切れる場合はFizz5で割り切れる場合はBuzz両方で割り切れる場合はFizzBuzzをプリントする")
     public void testPrintValue() {
-        List<String> list = FizzBuzz.generateList();
-        assertEquals("1", list.get(0));
-        assertEquals("Fizz", list.get(2));
-        assertEquals("Buzz", list.get(4));
-        assertEquals("FizzBuzz", list.get(14));
-        assertEquals("Buzz", list.get(99));
+        _fizzBuzzData.setValues(FizzBuzz.generateList());
+        assertEquals("1", _fizzBuzzData.getValues().get(0));
+        assertEquals("Fizz", _fizzBuzzData.getValues().get(2));
+        assertEquals("Buzz", _fizzBuzzData.getValues().get(4));
+        assertEquals("FizzBuzz", _fizzBuzzData.getValues().get(14));
+        assertEquals("Buzz", _fizzBuzzData.getValues().get(99));
     }
     @Test
     @DisplayName("値が3で割り切れる場合はFizzをプリントする")
     public void testGenerateFizz() {
-        assertEquals("Fizz", FizzBuzz.generate(3));
+        _fizzBuzzData.setValue(FizzBuzz.generate(3));
+        assertEquals("Fizz", _fizzBuzzData.getValue());
     }
     @Test
     @DisplayName("値が5で割り切れる場合はBuzzをプリントする")
     public void testGenerateBuzz() {
-        assertEquals("Buzz", FizzBuzz.generate(5));
+        _fizzBuzzData.setValue(FizzBuzz.generate(5));
+        assertEquals("Buzz", _fizzBuzzData.getValue());
     }
     @Test
     @DisplayName("値が3と5で割り切れる場合はFizzBuzzをプリントする")
     public void testGenerateFizzBuzz() {
-        assertEquals("FizzBuzz", FizzBuzz.generate(15));
+        _fizzBuzzData.setValue(FizzBuzz.generate(15));
+        assertEquals("FizzBuzz", _fizzBuzzData.getValue());
     }
 }
