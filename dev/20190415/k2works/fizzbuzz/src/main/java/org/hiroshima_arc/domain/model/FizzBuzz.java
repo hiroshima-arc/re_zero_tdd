7dfef7773ac47e9361a40d8c31266182e1bb7698
k2works <kakimomokuri@gmail.com>
2019-04-22 10:54:33 +0900

    refactor: コレクションオブジェクトの導入

--- dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/domain/model/FizzBuzz.java
+++ dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/domain/model/FizzBuzz.java
@@ -9,16 +9,18 @@ import java.util.stream.IntStream;
 public class FizzBuzz {
 
     private FizzBuzzValue _value;
-    private List<FizzBuzzValue> _values;
+    private FizzBuzzValues _values;
     private FizzBuzzType _type;
 
     public FizzBuzz() {
-        _values = new ArrayList<>();
+        List<FizzBuzzValue> list = new ArrayList<>();
+        _values = new FizzBuzzValues(list);
         _type = FizzBuzzType.valueOf("one");
     }
 
     public FizzBuzz(FizzBuzzType type) {
-        _values = new ArrayList<>();
+        List<FizzBuzzValue> list = new ArrayList<>();
+        _values = new FizzBuzzValues(list);
         this._type = type;
     }
 
@@ -27,15 +29,15 @@ public class FizzBuzz {
     }
 
     public List<FizzBuzzValue> getValues() {
-        return _values;
+        return _values.get_values();
     }
 
     public void printValue() {
-        _values.forEach(System.out::println);
+        _values.get_values().forEach(System.out::println);
     }
 
     public void generateList() {
-        IntStream.rangeClosed(1, 100).forEach(i -> _values.add(_type.generate(i)));
+        IntStream.rangeClosed(1, 100).forEach(i -> _values = _values.add(_type.generate(i)));
     }
 
     public String generate(int number) {
---
+++ dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/domain/model/FizzBuzzValues.java
@@ -0,0 +1,21 @@
+package org.hiroshima_arc.domain.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzzValues {
+    private final List<FizzBuzzValue> _values;
+
+    public FizzBuzzValues(List<FizzBuzzValue> values) {
+        this._values = values;
+    }
+    public List<FizzBuzzValue> get_values() {
+        return _values;
+    }
+
+    public FizzBuzzValues add(FizzBuzzValue value) {
+        List<FizzBuzzValue> result = new ArrayList<>(_values);
+        result.add(value);
+        return new FizzBuzzValues(result);
+    }
+}
