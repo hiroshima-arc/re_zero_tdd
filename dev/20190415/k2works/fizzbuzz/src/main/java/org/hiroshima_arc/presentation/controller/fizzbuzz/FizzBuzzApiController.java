be8f3c07e6e0eb6575c6edcf76cb554464474eae
k2works <kakimomokuri@gmail.com>
2019-04-26 11:41:27 +0900

    feat: FizzBuzzApi仮実装

--- dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/presentation/controller/fizzbuzz/FizzBuzzApiController.java
+++ dev/20190415/k2works/fizzbuzz/src/main/java/org/hiroshima_arc/presentation/controller/fizzbuzz/FizzBuzzApiController.java
@@ -1,6 +1,7 @@
 package org.hiroshima_arc.presentation.controller.fizzbuzz;
 
 import org.hiroshima_arc.application.service.fizzbuzz.FizzBuzzApiService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,8 @@ import java.util.List;
 
 @RestController
 public class FizzBuzzApiController {
-    @RequestMapping(value = "/api/fizzbuzz", method = RequestMethod.GET)
+    @CrossOrigin
+    @RequestMapping(value = "/api/fizzbuzz", produces = "application/json; charset=utf-8", method = RequestMethod.GET)
     public List fizzBuzzValues() {
         FizzBuzzApiService service = new FizzBuzzApiService();
         return service.execute(100);
