61c6ecdc34e36b541bd68854f8279101c4592c57
k2works <kakimomokuri@gmail.com>
2019-05-08 14:09:09 +0900

    feat: 3で割り切れる場合はFizzをプリントする

--- dev/20190508/k2works/src/index.php
+++ dev/20190508/k2works/src/index.php
@@ -147,6 +147,23 @@ class FizzBuzzTest extends TestCase
         $this->assertEquals(1, $list[0]);
         $this->assertEquals(100, $list[99]);
     }
+    /** @noinspection NonAsciiCharacters */
+    /**
+     * @test
+     */
+    public function 値が3で割り切れる場合はFizzをプリントする()
+    {
+        $this->assertEquals("Fizz", fizzBuzz(3));
+    }
+}
+
+function fizzBuzz($number)
+{
+    $result = $number;
+    if ($number % 3 == 0) {
+        $result = "Fizz";
+    }
+    return $result;
 }
 
 function printValue()
