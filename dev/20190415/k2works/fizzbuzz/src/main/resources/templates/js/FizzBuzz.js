3d4bfbdfc79a1562f2a78b6c522171012afd5ed7
k2works <kakimomokuri@gmail.com>
2019-05-08 10:14:48 +0900

    refactor: モジュールの抽出

--- dev/20190415/k2works/fizzbuzz/src/main/resources/templates/js/FizzBuzz.js
+++ dev/20190415/k2works/fizzbuzz/src/main/resources/templates/js/FizzBuzz.js
@@ -1,112 +1,3 @@
-function showFizzBuzz() {
-  let fizzBuzz = fizzBuzzFactory("one");
-  fizzBuzz.render();
-  document.querySelector("#app").style.display="block";
-  document.querySelector("#app-api").style.display="none";
-  document.querySelector("#mocha").style.display="none";
-  document.querySelector("#message").innerHTML = "";
-}
-
-function getApiUrl() {
-  let url = document.querySelector("#api-url").innerHTML;
-  if (url === "") url = "http://localhost:8080/api";
-  return url;
-}
-
-function showFizzBuzzApi() {
-  const url = getApiUrl();
-  renderFizzBuzzApiSelectBox();
-  apiCall(`${url}/fizzbuzz/one`, 'POST', renderFizzBuzzApi);
-  document.querySelector("#app").style.display="none";
-  document.querySelector("#app-api").style.display="block";
-  document.querySelector("#mocha").style.display="none";
-}
-
-function renderFizzBuzzApiSelectBox() {
-  let selectBox = `
-        <select name=”type” onchange="changeTypeByApiCall(event)">
-            <option value="one">タイプ1</option>
-            <option value="two">タイプ2</option>
-            <option value="three">タイプ3</option>
-        </select>
-      `;
-
-  const appApi = document.getElementById("app-api");
-  const appApiSelectBox = appApi.getElementsByClassName("selectBox");
-  appApiSelectBox[0].innerHTML = selectBox;
-}
-
-function renderFizzBuzzApi(values) {
-  const header = () => {
-    let header = "<thead>";
-    header += "<tr>";
-    [...Array(10).keys()].forEach((i) => header += `<th>${i + 1}</th>`);
-    header += "</tr>";
-    header += "</thead>";
-    return header;
-  };
-
-  const items = () => {
-    let count = 1;
-    let result = "<tr>";
-    values.forEach((value) => {
-      result += `<td>${value.value}</td>`;
-      if (count === 10) {
-        result += "</tr>";
-        result += "<tr>";
-        count = 1;
-      } else {
-        count++;
-      }
-    });
-
-    return result;
-  };
-
-  let table = `
-            <table>
-                ${header()}
-                <tbody>
-                ${items(values)}
-                </tbody>
-            </table>
-    `;
-
-  const appApi = document.getElementById("app-api");
-  const appApiTable = appApi.getElementsByClassName("table");
-  appApiTable[0].innerHTML = table;
-}
-
-function changeTypeByApiCall(e) {
-  const typeValue = e.target.value;
-  const url = getApiUrl();
-  const clearTable = () => {
-    document.querySelector("#app-api").style.display="none";
-  };
-
-  apiCall(`${url}/fizzbuzz/${typeValue}`, 'POST', renderFizzBuzzApi, clearTable);
-}
-
-function changeType(e) {
-  let typeValue = e.target.value;
-  let fizzBuzz = fizzBuzzFactory(typeValue);
-  document.querySelector(".table").innerHTML = fizzBuzz.renderTable();
-}
-
-function fizzBuzzFactory(type) {
-  const keys = Object.keys(Type);
-  switch (type) {
-    case keys[0]:
-      return new FizzBuzz(Type.one);
-    case keys[1]:
-      return new FizzBuzz(Type.two);
-    case keys[2]:
-      return new FizzBuzz(Type.three);
-    default:
-      return new FizzBuzz(type1);
-  }
-}
-
 const Type = {
   one: type1,
   two: type2,
@@ -144,9 +35,9 @@ function FizzBuzz(type) {
             <table>
                 <thead>
       `;
-    table += `<tr>`;
+    table += "<tr>";
     [...Array(10).keys()].forEach((i) => table += `<th>${i + 1}</th>`);
-    table += `</tr>`;
+    table += "</tr>";
     table += `
                 </thead>
                 <tbody>
@@ -156,13 +47,13 @@ function FizzBuzz(type) {
       let counter = 0;
       return {
         create(list) {
-          table += `<tr>`;
+          table += "<tr>";
           list.forEach((item) => {
             table += `<td>${item}</td>`;
             counter++;
             if (counter === 10) {
-              table += `</tr>`;
-              table += `<tr>`;
+              table += "</tr>";
+              table += "<tr>";
               counter = 0;
             }
           });
@@ -193,3 +84,33 @@ function FizzBuzz(type) {
     document.querySelector(".table").innerHTML = this.renderTable();
   };
 }
+
+function showFizzBuzz() {
+  let fizzBuzz = fizzBuzzFactory("one");
+  fizzBuzz.render();
+  document.querySelector("#app").style.display="block";
+  document.querySelector("#app-api").style.display="none";
+  document.querySelector("#mocha").style.display="none";
+  document.querySelector("#message").innerHTML = "";
+}
+
+function changeType(e) {
+  let typeValue = e.target.value;
+  let fizzBuzz = fizzBuzzFactory(typeValue);
+  document.querySelector(".table").innerHTML = fizzBuzz.renderTable();
+}
+
+function fizzBuzzFactory(type) {
+  const keys = Object.keys(Type);
+  switch (type) {
+  case keys[0]:
+    return new FizzBuzz(Type.one);
+  case keys[1]:
+    return new FizzBuzz(Type.two);
+  case keys[2]:
+    return new FizzBuzz(Type.three);
+  default:
+    return new FizzBuzz(type1);
+  }
+}
+
