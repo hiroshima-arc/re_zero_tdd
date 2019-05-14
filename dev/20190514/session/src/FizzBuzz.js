b0de87b3ad1ea4d2b787be512855f19af464c7d5
k2works <kakimomokuri@gmail.com>
2019-05-14 11:34:15 +0000

    関数名の変更

--- dev/20190514/session/src/FizzBuzz.js
+++ dev/20190514/session/src/FizzBuzz.js
@@ -1,7 +1,7 @@
     const assert = chai.assert;
     suite("FizzBuzzTest", () => {
         test("1から100までをプリントする", () => {
-            let list = print1to100();
+            let list = generateList(); 
             assert.equal(1, list[0]); 
             assert.equal(100, list[99]); 
         });
@@ -16,7 +16,7 @@
         })
     });
     
-    function print1to100() {
+    function generateList(){
         let list = [];
         for(let i = 1; i <= 100; i++) {
             list.push(i);
