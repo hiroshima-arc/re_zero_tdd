bb44f00919d373f08a1b0fe93953f3a7f33bf870
k2works <kakimomokuri@gmail.com>
2019-05-15 02:44:21 +0000

    FizzBuzz

--- dev/20190514/session/src/FizzBuzz.js
+++ dev/20190514/session/src/FizzBuzz.js
@@ -1,9 +1,12 @@
 const assert = chai.assert;
 suite("FizzBuzzTest", () => {
-  test("1から100までをプリントする", () => {
+  test("1から100までをプリントする,ただし3で割り切れる場合はFizz5で割り切れる場合はBuzz15で割り切れる場合はFizzBuzzをプリントする", () => {
     let list = generateList();
     assert.equal(1, list[0]);
-    assert.equal(100, list[99]);
+    assert.equal("Fizz", list[2]);
+    assert.equal("Buzz", list[4]);
+    assert.equal("FizzBuzz", list[14]);
+    assert.equal("Buzz", list[99]);
   });
   test("3で割り切れる場合はFizzをプリントする", () => {
     assert.equal("Fizz", fizzBuzz(3));
@@ -17,7 +20,7 @@ suite("FizzBuzzTest", () => {
 });
 
 function generateList() {
-  return [...Array(101).keys()].slice(1).map(ix => ix);
+  return [...Array(101).keys()].slice(1).map(ix => fizzBuzz(ix));
 }
 
 function fizzBuzz(number) {
