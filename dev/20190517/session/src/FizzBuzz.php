021d2ef608296a928179755c7be685db8b449ca9
k2works <kakimomokuri@gmail.com>
2019-05-17 10:36:48 +0000

    refactor: マジックナンバーからシンボル定数へ

--- dev/20190517/session/src/FizzBuzz.php
+++ dev/20190517/session/src/FizzBuzz.php
@@ -1,19 +1,24 @@
 <?php
 class FizzBuzz
 {
+    const FIZZ_BUZZ = "FizzBuzz";
+    const FIZZ = "Fizz";
+    const BUZZ = "Buzz";
+    const MAX_NUMBER = 100;
+
     public static function generate($number)
     {
         $isFizz = $number % 3 === 0;
         $isBuzz = $number % 5 === 0;
 
         if ($isFizz && $isBuzz) {
-            return "FizzBuzz";
+            return self::FIZZ_BUZZ;
         }
         if ($isFizz) {
-            return "Fizz";
+            return self::FIZZ;
         }
         if ($isBuzz) {
-            return "Buzz";
+            return self::BUZZ;
         }
         return $number;
     }
@@ -21,6 +26,6 @@ class FizzBuzz
     {
         return array_map(function ($number) {
             return self::generate($number);
-        }, range(1, 100));
+        }, range(1, self::MAX_NUMBER));
     }
 }
