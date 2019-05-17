f520b1d93a1bd45e42906fbb49f5351c2227c192
k2works <kakimomokuri@gmail.com>
2019-05-17 09:44:24 +0000

    refactor: 説明変数の導入

--- dev/20190517/session/src/FizzBuzz.php
+++ dev/20190517/session/src/FizzBuzz.php
@@ -2,13 +2,16 @@
 
 function fizzBuzz($number)
 {
-    if ($number % 3 === 0 && $number % 5 === 0) {
+    $isFizz = $number % 3 === 0;
+    $isBuzz = $number % 5 === 0;
+
+    if ($isFizz && $isBuzz) {
         return "FizzBuzz";
     }
-    if ($number % 3 === 0) {
+    if ($isFizz) {
         return "Fizz";
     }
-    if ($number % 5 === 0) {
+    if ($isBuzz) {
         return "Buzz";
     }
     return $number;
