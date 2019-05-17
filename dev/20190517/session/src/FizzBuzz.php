e6d70fe0927f4152354bd9920dbbc8e9e62d4ade
k2works <kakimomokuri@gmail.com>
2019-05-17 09:32:17 +0000

    refactor: 条件分岐のネストからガード節へ

---
+++ dev/20190517/session/src/FizzBuzz.php
@@ -0,0 +1,23 @@
+<?php
+
+function fizzBuzz($number)
+{
+    if ($number % 3 === 0 && $number % 5 === 0) {
+        return "FizzBuzz";
+    }
+    if ($number % 3 === 0) {
+        return "Fizz";
+    }
+    if ($number % 5 === 0) {
+        return "Buzz";
+    }
+    return $number;
+}
+function createList()
+{
+    $list = [];
+    for ($i = 1; $i <= 100; $i++) {
+        $list[] = fizzBuzz($i);
+    }
+    return $list;
+}
