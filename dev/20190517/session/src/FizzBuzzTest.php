49c5ba15e8d7ee508565571105d31632989340bd
k2works <kakimomokuri@gmail.com>
2019-05-17 07:46:31 +0000

    test: Hello World

--- dev/20190517/session/src/FizzBuzzTest.php
+++ dev/20190517/session/src/FizzBuzzTest.php
@@ -5,4 +5,13 @@ include "FizzBuzz.php";
 use PHPUnit\Framework\TestCase;
 class FizzBuzzTest extends TestCase
 {
+    public function testGreeting()
+    {
+        $this->assertEquals("Hello World", greeting());
+    }
+}
+
+function greeting()
+{
+    return "Hello World";
 }
