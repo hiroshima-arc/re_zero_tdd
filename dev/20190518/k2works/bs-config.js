6548253336e13ee4a460f61369ffde3091c554a3
k2works <kakimomokuri@gmail.com>
2019-05-18 14:49:34 +0900

    fix: Lint指摘事項反映

--- dev/20190518/k2works/bs-config.js
+++ dev/20190518/k2works/bs-config.js
@@ -13,99 +13,99 @@
  |
  */
 module.exports = {
-  "ui": {
-    "port": 3001
+  'ui': {
+    'port': 3001
   },
-  "files": "src/*",
-  "watchEvents": [
-    "change"
+  'files': 'src/*',
+  'watchEvents': [
+    'change'
   ],
-  "watch": true,
-  "ignore": [],
-  "single": false,
-  "watchOptions": {
-    "ignoreInitial": true
+  'watch': true,
+  'ignore': [],
+  'single': false,
+  'watchOptions': {
+    'ignoreInitial': true
   },
-  "server": {
-    baseDir: "./src",
-    index: "index.html"
+  'server': {
+    baseDir: './src',
+    index: 'index.html'
   },
-  "proxy": false,
-  "port": 3000,
-  "middleware": false,
-  "serveStatic": [],
-  "ghostMode": {
-    "clicks": true,
-    "scroll": true,
-    "location": true,
-    "forms": {
-      "submit": true,
-      "inputs": true,
-      "toggles": true
+  'proxy': false,
+  'port': 3000,
+  'middleware': false,
+  'serveStatic': [],
+  'ghostMode': {
+    'clicks': true,
+    'scroll': true,
+    'location': true,
+    'forms': {
+      'submit': true,
+      'inputs': true,
+      'toggles': true
     }
   },
-  "logLevel": "info",
-  "logPrefix": "Browsersync",
-  "logConnections": false,
-  "logFileChanges": true,
-  "logSnippet": true,
-  "rewriteRules": [],
-  "open": "local",
-  "browser": "default",
-  "cors": false,
-  "xip": false,
-  "hostnameSuffix": false,
-  "reloadOnRestart": false,
-  "notify": true,
-  "scrollProportionally": true,
-  "scrollThrottle": 0,
-  "scrollRestoreTechnique": "window.name",
-  "scrollElements": [],
-  "scrollElementMapping": [],
-  "reloadDelay": 0,
-  "reloadDebounce": 500,
-  "reloadThrottle": 0,
-  "plugins": [],
-  "injectChanges": true,
-  "startPath": null,
-  "minify": true,
-  "host": null,
-  "localOnly": false,
-  "codeSync": true,
-  "timestamps": true,
-  "clientEvents": [
-    "scroll",
-    "scroll:element",
-    "input:text",
-    "input:toggles",
-    "form:submit",
-    "form:reset",
-    "click"
+  'logLevel': 'info',
+  'logPrefix': 'Browsersync',
+  'logConnections': false,
+  'logFileChanges': true,
+  'logSnippet': true,
+  'rewriteRules': [],
+  'open': 'local',
+  'browser': 'default',
+  'cors': false,
+  'xip': false,
+  'hostnameSuffix': false,
+  'reloadOnRestart': false,
+  'notify': true,
+  'scrollProportionally': true,
+  'scrollThrottle': 0,
+  'scrollRestoreTechnique': 'window.name',
+  'scrollElements': [],
+  'scrollElementMapping': [],
+  'reloadDelay': 0,
+  'reloadDebounce': 500,
+  'reloadThrottle': 0,
+  'plugins': [],
+  'injectChanges': true,
+  'startPath': null,
+  'minify': true,
+  'host': null,
+  'localOnly': false,
+  'codeSync': true,
+  'timestamps': true,
+  'clientEvents': [
+    'scroll',
+    'scroll:element',
+    'input:text',
+    'input:toggles',
+    'form:submit',
+    'form:reset',
+    'click'
   ],
-  "socket": {
-    "socketIoOptions": {
-      "log": false
+  'socket': {
+    'socketIoOptions': {
+      'log': false
     },
-    "socketIoClientConfig": {
-      "reconnectionAttempts": 50
+    'socketIoClientConfig': {
+      'reconnectionAttempts': 50
     },
-    "path": "/browser-sync/socket.io",
-    "clientPath": "/browser-sync",
-    "namespace": "/browser-sync",
-    "clients": {
-      "heartbeatTimeout": 5000
+    'path': '/browser-sync/socket.io',
+    'clientPath': '/browser-sync',
+    'namespace': '/browser-sync',
+    'clients': {
+      'heartbeatTimeout': 5000
     }
   },
-  "tagNames": {
-    "less": "link",
-    "scss": "link",
-    "css": "link",
-    "jpg": "img",
-    "jpeg": "img",
-    "png": "img",
-    "svg": "img",
-    "gif": "img",
-    "js": "script"
+  'tagNames': {
+    'less': 'link',
+    'scss': 'link',
+    'css': 'link',
+    'jpg': 'img',
+    'jpeg': 'img',
+    'png': 'img',
+    'svg': 'img',
+    'gif': 'img',
+    'js': 'script'
   },
-  "injectNotification": false
-};
+  'injectNotification': false
+}
