c042a9ece273d5a35181125fb643b72affa0fac2
k2works <kakimomokuri@gmail.com>
2019-05-20 13:08:54 +0900

    chore: アプリケーションのセットアップ

---
+++ dev/20190520/k2works/api/src/test/java/k2works/JupiterTest.java
@@ -0,0 +1,16 @@
+package k2works;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JupiterTest {
+
+  @Test
+  public void ok() {
+    assertEquals(1, 1);
+    System.out.println("Hello from JUnit Jupiter!");
+  }
+
+}
+
