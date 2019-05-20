eb858e320dbfea07843d7c0b810656e6b375168e
k2works <kakimomokuri@gmail.com>
2019-05-20 10:54:46 +0900

    chore: アプリケーションのセットアップ

--- dev/20190520/k2works/src/test/java/k2works/AppTest.java
+++ dev/20190520/k2works/src/test/java/k2works/AppTest.java
@@ -3,11 +3,12 @@
  */
 package k2works;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test
+    void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
