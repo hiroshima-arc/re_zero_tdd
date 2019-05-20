ae358e8ddd2cd44a429cc67c9fbbd9e92aac5326
k2works <kakimomokuri@gmail.com>
2019-05-20 08:47:47 +0000

    feat: 値が15で割り切れる場合はFizzBuzzをプリントする

--- dev/20190520/k2works/api/src/main/java/k2works/FizzBuzz.java
+++ dev/20190520/k2works/api/src/main/java/k2works/FizzBuzz.java
@@ -4,7 +4,9 @@ public class FizzBuzz {
 
   public static String generate(int number) {
     String result = Integer.toString(number);
-    if (number % 3 == 0) {
+    if (number % 3 == 0 && number % 5 == 0) {
+      result = "FizzBuzz";
+    } else if (number % 3 == 0) {
       result = "Fizz";
     } else if (number % 5 == 0) {
       result = "Buzz";
