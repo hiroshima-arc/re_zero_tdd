745becf344ee10274646bf984ace4e98d00577f4
k2works <kakimomokuri@gmail.com>
2019-05-20 08:26:09 +0000

    feat: 1から100までの数をプリントできるようにする

--- dev/20190520/k2works/api/src/main/java/k2works/FizzBuzz.java
+++ dev/20190520/k2works/api/src/main/java/k2works/FizzBuzz.java
@@ -3,7 +3,11 @@ package k2works;
 public class FizzBuzz {
 
   public static int[] printValue() {
-    return new int[] {};
+    int[] list = new int[100];
+    for (int i = 1; i <= 100; i++) {
+      list[i - 1] = i;
+    }
+    return list;
   }
 
 }
