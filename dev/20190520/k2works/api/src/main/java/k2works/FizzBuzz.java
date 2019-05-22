2b0f34d2d012211d110e660a01b270b539c6c708
k2works <kakimomokuri@gmail.com>
2019-05-22 10:30:08 +0900

    refactor: シンボル定数によるマジックナンバーの置き換え

--- dev/20190520/k2works/api/src/main/java/k2works/FizzBuzz.java
+++ dev/20190520/k2works/api/src/main/java/k2works/FizzBuzz.java
@@ -6,24 +6,28 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
+  public static final int MAX_NUMBER = 100;
+  public static final String FIZZ_BUZZ = "FizzBuzz";
+  public static final String FIZZ = "Fizz";
+  public static final String BUZZ = "Buzz";
 
   public static String generate(int number) {
     boolean isFizz = number % 3 == 0;
     boolean isBuzz = number % 5 == 0;
 
     if (isFizz && isBuzz)
-    return "FizzBuzz";
+    return FIZZ_BUZZ;
     if (isFizz)
-    return "Fizz";
+    return FIZZ;
     if (isBuzz)
-    return "Buzz";
+    return BUZZ;
     return Integer.toString(number);
   }
 
   public static List<String> generateList() {
-    return IntStream.rangeClosed(1, 100).mapToObj(FizzBuzz::generate).collect(
-      Collectors.toList()
-    );
+    return IntStream.rangeClosed(1, MAX_NUMBER).mapToObj(
+      FizzBuzz::generate
+    ).collect(Collectors.toList());
   }
 
 }
