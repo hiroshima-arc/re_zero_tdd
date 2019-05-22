3a14ae4a048deae2bb5ee4edc0f6924703f6c1bd
k2works <kakimomokuri@gmail.com>
2019-05-22 17:26:52 +0900

    refactor: タイプごとのロジックをenumで表現する

--- dev/20190520/k2works/api/src/test/java/k2works/FizzBuzzTest.java
+++ dev/20190520/k2works/api/src/test/java/k2works/FizzBuzzTest.java
@@ -7,11 +7,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-  private FizzBuzz _fizzBuzz;
+  private FizzBuzz _type01;
+  private FizzBuzz _type02;
+  private FizzBuzz _type03;
 
   @BeforeEach
   void init() {
-    _fizzBuzz = new FizzBuzz(FizzBuzzType.one);
+    _type01 = new FizzBuzz(FizzBuzzType.one);
+    _type02 = new FizzBuzz(FizzBuzzType.two);
+    _type03 = new FizzBuzz(FizzBuzzType.three);
   }
 
   @DisplayName(
@@ -19,27 +23,27 @@ public class FizzBuzzTest {
   )
   @Test
   void testPrint1To100() {
-    _fizzBuzz.generateList();
-    assertEquals("1", _fizzBuzz.getValuse().get(0));
-    assertEquals("Buzz", _fizzBuzz.getValuse().get(99));
+    _type01.generateList();
+    assertEquals("1", _type01.getValuse().get(0));
+    assertEquals("Buzz", _type01.getValuse().get(99));
   }
 
   @DisplayName("3で割り切れる場合はFizzをプリントする")
   @Test
   void testFizz() {
-    assertEquals("Fizz", _fizzBuzz.generate(3));
+    assertEquals("Fizz", _type01.generate(3));
   }
 
   @DisplayName("5で割り切れる場合はBuzzをプリントする")
   @Test
   void testBuzz() {
-    assertEquals("Buzz", _fizzBuzz.generate(5));
+    assertEquals("Buzz", _type01.generate(5));
   }
 
   @DisplayName("15で割り切れる場合はFizzBuzzをプリントする")
   @Test
   void testFizzBuzz() {
-    assertEquals("FizzBuzz", _fizzBuzz.generate(15));
+    assertEquals("FizzBuzz", _type01.generate(15));
   }
 
   @DisplayName("タイプ1は通常のパターンの値を返す")
@@ -60,33 +64,29 @@ public class FizzBuzzTest {
   @DisplayName("タイプ2は数字のみのパターンの値を返す")
   @Test
   void testType2() {
-    FizzBuzz type02 = new FizzBuzz(FizzBuzzType.two);
-    assertEquals("3", type02.generate(3));
+    assertEquals("3", _type02.generate(3));
   }
 
   @DisplayName("タイプ2は数字のみのパターンのリストを返す")
   @Test
   void testType2Valuse() {
-    FizzBuzz type02 = new FizzBuzz(FizzBuzzType.two);
-    type02.generateList();
-    assertEquals("3", type02.getValuse().get(2));
+    _type02.generateList();
+    assertEquals("3", _type02.getValuse().get(2));
   }
 
   @DisplayName("タイプ3は15で割り切れる場合にFizzBuzzのパターンの値を返す")
   @Test
   void testType3() {
-    FizzBuzz type03 = new FizzBuzz(FizzBuzzType.three);
-    assertEquals("FizzBuzz", type03.generate(15));
-    assertEquals("3", type03.generate(3));
+    assertEquals("FizzBuzz", _type03.generate(15));
+    assertEquals("3", _type03.generate(3));
   }
 
   @DisplayName("タイプ3は15で割り切れる場合にFizzBuzzのパターンのリストを返す")
   @Test
   void testType3Valuse() {
-    FizzBuzz type03 = new FizzBuzz(FizzBuzzType.three);
-    type03.generateList();
-    assertEquals("FizzBuzz", type03.getValuse().get(14));
-    assertEquals("3", type03.getValuse().get(2));
+    _type03.generateList();
+    assertEquals("FizzBuzz", _type03.getValuse().get(14));
+    assertEquals("3", _type03.getValuse().get(2));
   }
 
 }
