20bdd832991067f585e9a8ef7934eb47030cb3c4
k2works <kakimomokuri@gmail.com>
2019-05-22 13:26:42 +0900

    refactor: メソッドオブジェクトによるメソッドの置き換え

--- dev/20190520/k2works/api/src/main/java/k2works/FizzBuzz.java
+++ dev/20190520/k2works/api/src/main/java/k2works/FizzBuzz.java
@@ -12,6 +12,7 @@ public class FizzBuzz {
   private List<String> _valuse;
   private FizzBuzzType01 _type01;
   private FizzBuzzType02 _type02;
+  private FizzBuzzType03 _type03;
 
   public List<String> getValuse() {
     return _valuse;
@@ -21,6 +22,7 @@ public class FizzBuzz {
     this._valuse = new ArrayList<>();
     this._type01 = new FizzBuzzType01();
     this._type02 = new FizzBuzzType02();
+    this._type03 = new FizzBuzzType03();
   }
 
   public static String generate(int number) {
@@ -43,11 +45,7 @@ public class FizzBuzz {
       case 2:
         return this._type02.generate(number);
       case 3:
-        boolean isFizz = number % 3 == 0;
-        boolean isBuzz = number % 5 == 0;
-        if (isFizz && isBuzz)
-        return FIZZ_BUZZ;
-        return Integer.toString(number);
+        return this._type03.generate(number);
       default:
         return FizzBuzz.generate(number);
     }
---
+++ dev/20190520/k2works/api/src/main/java/k2works/FizzBuzzType03.java
@@ -0,0 +1,15 @@
+package k2works;
+
+public class FizzBuzzType03 {
+  private static final String FIZZ_BUZZ = "FizzBuzz";
+
+  public String generate(int number) {
+    boolean isFizz = number % 3 == 0;
+    boolean isBuzz = number % 5 == 0;
+    if (isFizz && isBuzz)
+    return FIZZ_BUZZ;
+    return Integer.toString(number);
+  }
+
+}
+
