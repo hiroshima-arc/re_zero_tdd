ff00825e0068fd35dad814c67b22f869aee53d8b
k2works <kakimomokuri@gmail.com>
2019-05-22 11:10:09 +0900

    refactor: データクラスによるレコードの置き換え

---
+++ dev/20190520/k2works/api/src/main/java/k2works/FizzBuzzData.java
@@ -0,0 +1,17 @@
+package k2works;
+
+import java.util.List;
+
+public class FizzBuzzData {
+  private List<String> _valuse;
+
+  public List<String> getValuse() {
+    return _valuse;
+  }
+
+  public void setValues(List<String> valuse) {
+    this._valuse = valuse;
+  }
+
+}
+
--- dev/20190520/k2works/api/src/test/java/k2works/FizzBuzzTest.java
+++ dev/20190520/k2works/api/src/test/java/k2works/FizzBuzzTest.java
@@ -2,12 +2,19 @@ package k2works;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+  private FizzBuzzData _data;
+
+  @BeforeEach
+  void init() {
+    _data = new FizzBuzzData();
+  }
 
   @DisplayName(
     "値を1から100までプリントする,ただし3で割り切れる場合はFizz5で割り切れる場合はBuzz15で割り切れる場合はFizzBuzzをプリントする"
@@ -15,8 +22,9 @@ public class FizzBuzzTest {
   @Test
   void testPrint1To100() {
     List<String> list = FizzBuzz.generateList();
-    assertEquals("1", list.get(0));
-    assertEquals("Buzz", list.get(99));
+    _data.setValues(list);
+    assertEquals("1", _data.getValuse().get(0));
+    assertEquals("Buzz", _data.getValuse().get(99));
   }
 
   @DisplayName("3で割り切れる場合はFizzをプリントする")
