3d0f804b100c64a864eb48a9824b84497bf846b9
k2works <kakimomokuri@gmail.com>
2019-05-24 20:05:00 +0900

    refactor: アルゴリズムの取り替え

--- dev/20190524/session/api/src/main/java/fizzbuzz/FizzBuzz.java
+++ dev/20190524/session/api/src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,11 +1,14 @@
 package fizzbuzz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
 
-  public static String[] generateList() {
-    String[] list = new String[100];
+  public static List<String> generateList() {
+    List<String> list = new ArrayList<>();
     for (int i = 1; i <= 100; i++) {
-      list[i - 1] = generate(i);
+      list.add(generate(i));
     }
     return list;
   }
--- dev/20190524/session/api/src/main/java/fizzbuzz/FizzBuzzController.java
+++ dev/20190524/session/api/src/main/java/fizzbuzz/FizzBuzzController.java
@@ -1,5 +1,7 @@
 package fizzbuzz;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,8 @@ public class FizzBuzzController {
 
   @CrossOrigin
   @RequestMapping("/fizzbuzz")
-  public String[] fizzbuzz() {
-    String[] list = FizzBuzz.generateList();
+  public List<String> fizzbuzz() {
+    List<String> list = FizzBuzz.generateList();
     return list;
   }
 
