261ea9603347e711bdd220eedcad9dee29c695fd
k2works <kakimomokuri@gmail.com>
2019-05-24 19:13:07 +0900

    test: FizzBuzzApi仮実装

--- dev/20190524/session/api/src/main/java/fizzbuzz/FizzBuzzController.java
+++ dev/20190524/session/api/src/main/java/fizzbuzz/FizzBuzzController.java
@@ -1,14 +1,16 @@
 package fizzbuzz;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class FizzBuzzController {
 
+  @CrossOrigin
   @RequestMapping("/fizzbuzz")
   public List fizzbuzz() {
     List<String> list = new ArrayList<>();
