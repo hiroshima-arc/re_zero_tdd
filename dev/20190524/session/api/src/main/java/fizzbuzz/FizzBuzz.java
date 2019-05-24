eb50b6ef613507b4267f3417542cf63c4fa3ddc5
k2works <kakimomokuri@gmail.com>
2019-05-24 20:07:45 +0900

    refactor: アルゴリズムの取り替え

--- dev/20190524/session/api/src/main/java/fizzbuzz/FizzBuzz.java
+++ dev/20190524/session/api/src/main/java/fizzbuzz/FizzBuzz.java
@@ -2,14 +2,17 @@ package fizzbuzz;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
   public static List<String> generateList() {
     List<String> list = new ArrayList<>();
-    for (int i = 1; i <= 100; i++) {
-      list.add(generate(i));
-    }
+    IntStream.rangeClosed(1, 100).forEach(
+      number -> {
+        list.add(FizzBuzz.generate(number));
+      }
+    );
     return list;
   }
 
