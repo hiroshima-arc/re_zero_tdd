bd964281d6c1ed5124b57b14fb316175c80e1f5d
k2works <kakimomokuri@gmail.com>
2019-05-30 18:56:23 +0900

    refactor: ループからパイプラインへ

--- dev/20190531/session/api/fizzbuzz/src/main/java/fizzbuzz/FizzBuzz.java
+++ dev/20190531/session/api/fizzbuzz/src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,19 +1,15 @@
 package fizzbuzz;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
   public static List<String> generateList() {
-    List<String> list = new ArrayList<>();
-    IntStream.rangeClosed(1, 100).forEach(
-      number -> {
-        list.add(FizzBuzz.generate(number));
-      }
+    return IntStream.rangeClosed(1, 100).mapToObj(FizzBuzz::generate).collect(
+      Collectors.toList()
     );
-    return list;
   }
 
   public static String generate(int number) {
