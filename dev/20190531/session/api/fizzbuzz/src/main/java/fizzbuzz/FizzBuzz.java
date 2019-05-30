3c0d110907a608a332c2dfd7904a40c1f33fe96b
k2works <kakimomokuri@gmail.com>
2019-05-30 18:41:37 +0900

    refactor: ガード節による入れ子条件記述の置き換え

--- dev/20190531/session/api/fizzbuzz/src/main/java/fizzbuzz/FizzBuzz.java
+++ dev/20190531/session/api/fizzbuzz/src/main/java/fizzbuzz/FizzBuzz.java
@@ -14,11 +14,13 @@ public class FizzBuzz {
     String result = Integer.toString(number);
 
     if (number % 3 == 0 && number % 5 == 0) {
-      result = "FizzBuzz";
-    } else if (number % 3 == 0) {
-      result = "Fizz";
-    } else if (number % 5 == 0) {
-      result = "Buzz";
+      return "FizzBuzz";
+    }
+    if (number % 3 == 0) {
+      return "Fizz";
+    }
+    if (number % 5 == 0) {
+      return "Buzz";
     }
     return result;
   }
