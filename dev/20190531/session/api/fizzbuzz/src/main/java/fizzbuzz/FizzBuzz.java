893bda3a7f0e0f4d5415f231bc432d0a173432c2
k2works <kakimomokuri@gmail.com>
2019-05-31 13:06:43 +0900

    feat: タイプ3

--- dev/20190531/session/api/fizzbuzz/src/main/java/fizzbuzz/FizzBuzz.java
+++ dev/20190531/session/api/fizzbuzz/src/main/java/fizzbuzz/FizzBuzz.java
@@ -44,6 +44,13 @@ public class FizzBuzz {
         return FizzBuzz.generate(number);
       case 2:
         return Integer.toString(number);
+      case 3:
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+        if (isFizz && isBuzz) {
+          return FIZZ_BUZZ;
+        }
+        return Integer.toString(number);
       default:
         return FizzBuzz.generate(number);
     }
