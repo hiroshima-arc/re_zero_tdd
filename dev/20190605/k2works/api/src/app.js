bd5be0367ea6441d42a91afb4bc1bba431858867
k2works <kakimomokuri@gmail.com>
2019-06-05 16:18:50 +0900

    chore: アプリケーションのセットアップ

--- dev/20190605/k2works/api/src/app.js
+++ dev/20190605/k2works/api/src/app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const HelloController = require('./controller/HelloController');
 
+// CORSを許可する
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 const API = '/api';
 
 app.use(API, HelloController);
