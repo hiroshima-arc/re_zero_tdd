60f95ec278408093542fec2789a341fc079559d6
k2works <kakimomokuri@gmail.com>
2019-06-05 14:01:57 +0900

    chore: アプリケーションのセットアップ

--- dev/20190605/k2works/.eslintrc.js
+++ dev/20190605/k2works/.eslintrc.js
@@ -13,5 +13,11 @@ module.exports = {
     'ecmaVersion': 2018
   },
   'rules': {
+    "semi": ["error", "always"],
+    "semi-spacing": ["error", {"after": true, "before": false}],
+    "semi-style": ["error", "last"],
+    "no-extra-semi": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error"
   }
 }
