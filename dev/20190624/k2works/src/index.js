055ec8d860713c0bf585bcfea537fccef87852aa
k2works <kakimomokuri@gmail.com>
2019-06-25 04:28:54 +0000

    feat: The Starting Point

--- dev/20190624/k2works/src/index.js
+++ dev/20190624/k2works/src/index.js
@@ -1,3 +1,4 @@
-export function greeting() {
-  return "hello, world";
-}
+import { invoices, plays, statement } from "./statement";
+const textStatement = statement(invoices[0], plays);
+console.log(textStatement);
+document.querySelector("#app").innerHTML = `<pre>${textStatement}</pre>`;
--- dev/20190624/k2works/test/index.test.js
+++ dev/20190624/k2works/test/index.test.js
@@ -1,7 +1,7 @@
 import { expect } from "chai";
-import {greeting} from '../src/index';
-describe("HelloWorld", () => {
-  it("greeting", () => {
-    expect("hello, world").to.equal(greeting());
-  });
-});
+import {greeting, statement, invoices, plays} from '../src/statement';
+describe("Volume Credits", ()=> {
+  it("print text statement", () => {
+    expect('Stagement for BigCo\n Hamlet: $650.00(55 seats)\n As You Like It: $580.00(35 seats)\n Othello: $500.00(40 seats)\nAmount owed is $1,730.00\nYou earned 40 creddits\n').to.equal(statement(invoices[0], plays))
+  })
+})
