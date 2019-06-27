10b649d4bfe1f3de32fe51512db4a69f016ceadf
k2works <kakimomokuri@gmail.com>
2019-06-27 10:19:15 +0000

    refactor: Extract Function

--- dev/20190624/k2works/src/statement.js
+++ dev/20190624/k2works/src/statement.js
@@ -33,11 +33,7 @@ export function statement(invoice, plays) {
     minimumFractionDigits: 2
   }).format;
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)}(${
@@ -73,5 +69,12 @@ export function statement(invoice, plays) {
   }
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
   }
 }
