85987dd7a47134b3c8f7779efef2c4d2f14d93da
k2works <kakimomokuri@gmail.com>
2019-06-27 08:43:38 +0000

    refactor: Rename Variable

--- dev/20190624/k2works/src/statement.js
+++ dev/20190624/k2works/src/statement.js
@@ -51,22 +51,22 @@ export function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  function amountFor(perf, play) {
+  function amountFor(aPerformace, play) {
     let result = 0;
 
     switch (play.type) {
       case "tragedy":
         result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30);
+        if (aPerformace.audience > 30) {
+          result += 1000 * (aPerformace.audience - 30);
         }
         break;
       case "comedy":
         result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20);
+        if (aPerformace.audience > 20) {
+          result += 10000 + 500 * (aPerformace.audience - 20);
         }
-        result += 300 * perf.audience;
+        result += 300 * aPerformace.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
