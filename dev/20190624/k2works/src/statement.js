d013d1665ab947cd857a759e2d2721b96d1aa6ef
k2works <kakimomokuri@gmail.com>
2019-06-27 09:49:54 +0000

    refactor: Extract Function

--- dev/20190624/k2works/src/statement.js
+++ dev/20190624/k2works/src/statement.js
@@ -33,7 +33,7 @@ export function statement(invoice, plays) {
     minimumFractionDigits: 2
   }).format;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -72,5 +72,8 @@ export function statement(invoice, plays) {
         throw new Error(`unknown type: ${play.type}`);
     }
     return result;
+  }
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
