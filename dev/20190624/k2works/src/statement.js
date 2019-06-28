b811dc5cbdc3163f23c2e01aef9bec1c93bef369
k2works <kakimomokuri@gmail.com>
2019-06-28 04:47:43 +0000

    refactor: Move Function

--- dev/20190624/k2works/src/statement.js
+++ dev/20190624/k2works/src/statement.js
@@ -31,7 +31,12 @@ export function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
