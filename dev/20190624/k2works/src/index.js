b7a58379071239127c305d949fca9f08caea9b57
k2works <kakimomokuri@gmail.com>
2019-06-28 05:45:06 +0000

    feat: HtmlStatement

--- dev/20190624/k2works/src/index.js
+++ dev/20190624/k2works/src/index.js
@@ -1,4 +1,7 @@
-import { invoices, plays, statement } from "./statement";
+import { invoices, plays, statement, htmlStatement } from "./statement";
 const textStatement = statement(invoices[0], plays);
 console.log(textStatement);
-document.querySelector("#app").innerHTML = `<pre>${textStatement}</pre>`;
+document.querySelector("#app").innerHTML = `<pre>${htmlStatement(
+  invoices[0],
+  plays
+)}</pre>`;
