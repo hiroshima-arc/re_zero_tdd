ea6963c045b2c7834482b02e6672a5ccf83f0d9d
k2works <kakimomokuri@gmail.com>
2019-06-28 05:16:04 +0000

    refactor: Replace Loop with Pipeline

--- dev/20190624/k2works/src/statement.js
+++ dev/20190624/k2works/src/statement.js
@@ -71,18 +71,10 @@ export function statement(invoice, plays) {
     return result;
   }
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
