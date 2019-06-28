f81cd1fbf22bdf96498748fec6f5f3c6305b62ea
k2works <kakimomokuri@gmail.com>
2019-06-28 03:39:11 +0000

    refactor: Change Function Declaration

--- dev/20190624/k2works/src/statement.js
+++ dev/20190624/k2works/src/statement.js
@@ -31,12 +31,12 @@ export function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)}(${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))}(${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -77,6 +77,6 @@ export function statement(invoice, plays) {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 }
