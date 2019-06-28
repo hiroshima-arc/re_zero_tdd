e5d441072c047a6adddc4c3563fe61f5983363b6
k2works <kakimomokuri@gmail.com>
2019-06-28 04:17:07 +0000

    refactor: Rename Varialbe

--- dev/20190624/k2works/src/statement.js
+++ dev/20190624/k2works/src/statement.js
@@ -75,17 +75,17 @@ export function statement(invoice, plays) {
     }).format(aNumber / 100);
   }
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
   function totalAmount() {
-    let totalAmount = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf);
+      result += amountFor(perf);
     }
-    return totalAmount;
+    return result;
   }
 }
