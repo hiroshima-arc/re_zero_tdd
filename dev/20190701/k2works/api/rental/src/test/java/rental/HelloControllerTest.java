46495a4b5b9e26cfe8e61e3576707941b09ddbf9
k2works <kakimomokuri@gmail.com>
2019-07-01 13:50:56 +0900

    chore: アプリケーションのセットアップ

--- dev/20190701/k2works/api/rental/src/test/java/rental/HelloControllerTest.java
+++ dev/20190701/k2works/api/rental/src/test/java/rental/HelloControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,15 +17,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
+  @Autowired
+  private MockMvc mvc;
 
-    @Autowired
-    private MockMvc mvc;
-
-    @Test
-    public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
-    }
+  @Test
+  public void getHello() throws Exception {
+    mvc.perform(
+      MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)
+    )
+      .andExpect(status().isOk())
+      .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+  }
 }
-
