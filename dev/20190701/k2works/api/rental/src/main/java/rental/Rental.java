325cd8f2e13f6c649d45b1b1df6c7c11584f045a
k2works <kakimomokuri@gmail.com>
2019-07-01 16:43:37 +0900

    refactor: 破壊的代入から、目的別変数へ

--- dev/20190701/k2works/api/rental/src/main/java/rental/Rental.java
+++ dev/20190701/k2works/api/rental/src/main/java/rental/Rental.java
@@ -20,26 +20,28 @@ class Rental {
   }
 
   int amount() {
-    int amount = 0;
+    int baseAmount = 0;
+    int additionalAmount = 0;
     switch (movie().movieType()) {
       case REGULAR:
-        amount += 200;
-        if (daysRented() > 2) amount += (daysRented() - 2) * 150;
+        baseAmount = 200;
+        if (daysRented() > 2) additionalAmount = (daysRented - 2) * 150;
         break;
       case NEW_RELEASE:
-        amount += daysRented() * 300;
+        baseAmount = 0;
+        additionalAmount = daysRented() * 300;
         break;
       case CHILDREN:
-        amount += 150;
-        if (daysRented() > 3) amount += (daysRented() - 3) * 150;
+        baseAmount = 150;
+        if (daysRented() > 3) additionalAmount = (daysRented - 3) * 150;
     }
 
-    return amount;
+    return baseAmount + additionalAmount;
   }
 
   int frequentPoints() {
     int points = 1;
-    if (movie().movieType() == NEW_RELEASE && daysRented() > 1) points++;
+    if (movie().movieType() == NEW_RELEASE && daysRented > 1) points++;
 
     return points;
   }
