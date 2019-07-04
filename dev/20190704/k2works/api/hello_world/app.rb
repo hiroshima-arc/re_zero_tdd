9b80cc32e0c1fd6fca78f79e7d80e6bddc222338
k2works <kakimomokuri@gmail.com>
2019-07-04 14:54:06 +0900

    chore: アプリケーションのセットアップ

--- dev/20190704/k2works/api/hello_world/app.rb
+++ dev/20190704/k2works/api/hello_world/app.rb
@@ -31,8 +31,8 @@ def lambda_handler(event:, context:)
   {
     statusCode: 200,
     body: {
-      message: "Hello World!",
       # location: response.body
+      message: 'Hello World!'
     }.to_json
   }
 end
