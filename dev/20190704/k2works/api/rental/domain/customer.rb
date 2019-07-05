9821dbb23c51a66d683668b95385fe85a7385239
k2works <kakimomokuri@gmail.com>
2019-07-05 13:54:10 +0900

    refactor: メソッドの抽出

--- dev/20190704/k2works/api/rental/domain/customer.rb
+++ dev/20190704/k2works/api/rental/domain/customer.rb
@@ -15,13 +15,7 @@ module Domain
       total_amount, frequent_renter_points = 0, 0
       result = "#{@name}様のレンタル明細\n"
       @rentals.each do |element|
-        # レンタルポイントを加算
-        frequent_renter_points += 1
-        # 新作２日間レンタルでボーナス点を加算
-        if element.movie.price_code == Movie::NEW_RELEASE &&
-            element.days_rented > 1
-          frequent_renter_points += 1
-        end
+        frequent_renter_points = frequent_renter_point(element, frequent_renter_points)
         # このレンタルの料金を表示
         result += "\t" + element.movie.title + "\t" + element.charge.to_s + "円\n"
         total_amount += element.charge
@@ -30,6 +24,19 @@ module Domain
       result += "合計金額 #{total_amount}円\n"
       result += "獲得ポイント #{frequent_renter_points}ポイント"
       result
+    end
+
+    private
+
+    def frequent_renter_point(element, frequent_renter_points)
+      # レンタルポイントを加算
+      frequent_renter_points += 1
+      # 新作２日間レンタルでボーナス点を加算
+      if element.movie.price_code == Movie::NEW_RELEASE &&
+          element.days_rented > 1
+        frequent_renter_points += 1
+      end
+      frequent_renter_points
     end
   end
 end
