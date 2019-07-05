1a3283be0fb1181c92e456cb367d148f183b90b2
k2works <kakimomokuri@gmail.com>
2019-07-05 13:09:34 +0900

    refactor: メソッドの抽出

--- dev/20190704/k2works/api/rental/domain/customer.rb
+++ dev/20190704/k2works/api/rental/domain/customer.rb
@@ -15,23 +15,7 @@ module Domain
       total_amount, frequent_renter_points = 0, 0
       result = "#{@name}様のレンタル明細\n"
       @rentals.each do |element|
-        this_amount = 0
-
-        # 各行の金額を計算
-        case element.movie.price_code
-        when Movie::REGULAR
-          this_amount += 200
-          if element.days_rented > 2
-            this_amount += (element.days_rented - 2) * 1.5
-          end
-        when Movie::NEW_RELEASE
-          this_amount += element.days_rented * 300
-        when Movie::CHILDREN
-          this_amount += 150
-          if element.days_rented > 3
-            this_amount += (element.days_rented - 3) * 1.5
-          end
-        end
+        this_amount = amount_for(element)
 
         # レンタルポイントを加算
         frequent_renter_points += 1
@@ -48,6 +32,27 @@ module Domain
       result += "合計金額 #{total_amount}円\n"
       result += "獲得ポイント #{frequent_renter_points}ポイント"
       result
+    end
+
+    private
+
+    def amount_for(element)
+      this_amount = 0
+      case element.movie.price_code
+      when Movie::REGULAR
+        this_amount += 200
+        if element.days_rented > 2
+          this_amount += (element.days_rented - 2) * 1.5
+        end
+      when Movie::NEW_RELEASE
+        this_amount += element.days_rented * 300
+      when Movie::CHILDREN
+        this_amount += 150
+        if element.days_rented > 3
+          this_amount += (element.days_rented - 3) * 1.5
+        end
+      end
+      this_amount
     end
   end
 end
