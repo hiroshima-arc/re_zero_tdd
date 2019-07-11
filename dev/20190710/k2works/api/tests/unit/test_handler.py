3ce7dff6570d452a3e9291d865bd94d83eb03835
k2works <kakimomokuri@gmail.com>
2019-07-11 14:13:42 +0900

    chore: アプリケーションのセットアップ

--- dev/20190710/k2works/api/tests/unit/test_handler.py
+++ dev/20190710/k2works/api/tests/unit/test_handler.py
@@ -56,9 +56,9 @@ def apigw_event():
             "Accept-Encoding": "gzip, deflate, sdch",
         },
         "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
+        "httpMethod": "GET",
         "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
+        "path": "/api/hello",
     }
 
 
@@ -67,7 +67,6 @@ def test_lambda_handler(apigw_event, mocker):
     ret = app.lambda_handler(apigw_event, "")
     data = json.loads(ret["body"])
 
-    assert ret["statusCode"] == 200
+    assert ret["statusCode"] == '200'
     assert "message" in ret["body"]
     assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
