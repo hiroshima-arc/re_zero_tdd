4c411382a5ecb345f14fef82d61f3ec344af03bc
k2works <kakimomokuri@gmail.com>
2019-07-13 17:27:28 +0900

    refactor: メソッドの移動

--- dev/20190710/k2works/api/video_rental/domain/movie.py
+++ dev/20190710/k2works/api/video_rental/domain/movie.py
@@ -17,3 +17,18 @@ class Movie:
     @property
     def price_code(self):
         return self.__price_code
+
+    def charge(self, days_rented):
+        result = 0
+        # 各行の金額を計算
+        if self.price_code == Movie.REGULAR:
+            result += 200
+            if days_rented > 2:
+                result += (days_rented - 2) * 1.5
+        elif self.price_code == Movie.NEW_RELEASE:
+            result += days_rented * 300
+        elif self.price_code == Movie.CHILDREN:
+            result += 150
+            if days_rented > 3:
+                result += (days_rented - 3) * 1.5
+        return result
--- dev/20190710/k2works/api/video_rental/domain/rental.py
+++ dev/20190710/k2works/api/video_rental/domain/rental.py
@@ -16,19 +16,7 @@ class Rental:
         return self.__days_rented
 
     def charge(self):
-        result = 0
-        # 各行の金額を計算
-        if self.movie.price_code == Movie.REGULAR:
-            result += 200
-            if self.days_rented > 2:
-                result += (self.days_rented - 2) * 1.5
-        elif self.movie.price_code == Movie.NEW_RELEASE:
-            result += self.days_rented * 300
-        elif self.movie.price_code == Movie.CHILDREN:
-            result += 150
-            if self.days_rented > 3:
-                result += (self.days_rented - 3) * 1.5
-        return result
+        return self.movie.charge(self.days_rented)
 
     def frequent_renter_point(self):
         return 2 if self.movie.price_code == Movie.NEW_RELEASE and self.days_rented else 1
