ac4ef80a8b0fa45e9349497dae0655ababce7006
k2works <kakimomokuri@gmail.com>
2019-07-13 16:30:57 +0900

    refactor: 一時変数から問い合わせメソッドへ

--- dev/20190710/k2works/api/video_rental/domain/customer.py
+++ dev/20190710/k2works/api/video_rental/domain/customer.py
@@ -17,16 +17,20 @@ class Customer:
     def statement(self):
         result = f"{self.name}様のレンタル明細\n"
 
-        frequent_renter_points = 0
         for element in self.__rentals:
-            frequent_renter_points += element.frequent_renter_point()
             # このレンタルの料金を表示
             result += f"\t{element.movie.title}\t{str(element.charge())}円\n"
 
         # フッター行を追加
         result += f"合計金額 {self.total_charge()}円\n"
-        result += f"獲得ポイント {frequent_renter_points}ポイント"
+        result += f"獲得ポイント {self.total_frequent_renter_points()}ポイント"
         return result
+
+    def total_frequent_renter_points(self):
+        total_point = 0
+        for element in self.__rentals:
+            total_point += element.frequent_renter_point()
+        return total_point
 
     def total_charge(self):
         total_amount = 0
