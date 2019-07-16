35d110e8573bdfbe03fb80500d06e9d36b3ad9d0
k2works <kakimomokuri@gmail.com>
2019-07-16 15:44:23 +0900

    feat: PutTodoItemメソッドの追加

--- dev/20190716/k2works/api/todo/src/todo/Controllers/TodoController.cs
+++ dev/20190716/k2works/api/todo/src/todo/Controllers/TodoController.cs
@@ -54,5 +54,20 @@ namespace todo.Controllers
 
             return CreatedAtAction(nameof(GetTodoItem), new {id = item.Id}, item);
         }
+        
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
