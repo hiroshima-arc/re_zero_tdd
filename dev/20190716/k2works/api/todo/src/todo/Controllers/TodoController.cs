37f3e279378f2fa7d0fbd06359363747170897de
k2works <kakimomokuri@gmail.com>
2019-07-16 15:38:21 +0900

    feat: Createメソッドの追加

--- dev/20190716/k2works/api/todo/src/todo/Controllers/TodoController.cs
+++ dev/20190716/k2works/api/todo/src/todo/Controllers/TodoController.cs
@@ -44,5 +44,15 @@ namespace todo.Controllers
 
             return todoItem;
         }
+        
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new {id = item.Id}, item);
+        }
     }
 }
