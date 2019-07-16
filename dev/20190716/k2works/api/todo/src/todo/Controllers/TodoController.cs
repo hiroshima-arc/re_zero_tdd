168faa212e3da7709140f95699826edc607983b1
k2works <kakimomokuri@gmail.com>
2019-07-16 15:50:09 +0900

    feat: DeleteTodoItemメソッドの追加

--- dev/20190716/k2works/api/todo/src/todo/Controllers/TodoController.cs
+++ dev/20190716/k2works/api/todo/src/todo/Controllers/TodoController.cs
@@ -69,5 +69,22 @@ namespace todo.Controllers
 
             return NoContent();
         }
+        
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
