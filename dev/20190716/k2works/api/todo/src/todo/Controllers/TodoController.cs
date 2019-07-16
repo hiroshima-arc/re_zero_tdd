2583ebe0a91ec8002f92d43bdaecaafd25c257a5
k2works <kakimomokuri@gmail.com>
2019-07-16 15:19:13 +0900

    feat: Get メソッドの追加

--- dev/20190716/k2works/api/todo/src/todo/Controllers/TodoController.cs
+++ dev/20190716/k2works/api/todo/src/todo/Controllers/TodoController.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using todo.Models;
 
 namespace todo.Controllers
@@ -21,5 +24,25 @@ namespace todo.Controllers
             }
         }
 
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/Todo
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
     }
 }
