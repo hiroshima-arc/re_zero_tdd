dc32dbc1721ce0861975e1a69c8dfffcf27c3cf8
k2works <kakimomokuri@gmail.com>
2019-07-18 13:54:04 +0900

    feat: 検証の追加

--- dev/20190716/k2works/app/MvcMovie/Models/Movie.cs
+++ dev/20190716/k2works/app/MvcMovie/Models/Movie.cs
@@ -7,18 +7,34 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Display(Name = "タイトル")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "未入力です")]
         public string Title { get; set; }
 
         [Display(Name = "公開日")]
-        [DataType(DataType.Date)] public DateTime ReleaseDate { get; set; }
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "未入力です")]
+        public DateTime ReleaseDate { get; set; }
+
         [Display(Name = "ジャンル")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]+$")]
+        [Required(ErrorMessage = "未入力です")]
+        [StringLength(30)]
         public string Genre { get; set; }
+
         [Display(Name = "値段")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
+        [Required(ErrorMessage = "未入力です")]
         public decimal Price { get; set; }
+
         [Display(Name = "評価")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required(ErrorMessage = "未入力です")]
         public string Rating { get; set; }
     }
 }
-
