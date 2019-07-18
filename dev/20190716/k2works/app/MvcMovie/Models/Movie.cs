c0a3cedc7e5f174836a30d1883cb2cbd4365137e
k2works <kakimomokuri@gmail.com>
2019-07-18 12:11:49 +0900

    コントローラーアクションとビュー

--- dev/20190716/k2works/app/MvcMovie/Models/Movie.cs
+++ dev/20190716/k2works/app/MvcMovie/Models/Movie.cs
@@ -1,15 +1,21 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Display(Name = "タイトル")]
         public string Title { get; set; }
 
+        [Display(Name = "公開日")]
         [DataType(DataType.Date)] public DateTime ReleaseDate { get; set; }
+        [Display(Name = "ジャンル")]
         public string Genre { get; set; }
+        [Display(Name = "値段")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
