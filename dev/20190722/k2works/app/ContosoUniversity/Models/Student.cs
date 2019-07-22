5850f878728c444d6dfa77cc69d4edcf4a9f00f7
k2works <kakimomokuri@gmail.com>
2019-07-22 16:24:22 +0900

    feat: データモデルを作成する

---
+++ dev/20190722/k2works/app/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Data;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DataSetDateTime EnrollmentData { get; set; }
+        
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
