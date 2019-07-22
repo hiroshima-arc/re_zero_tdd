23e98c12cae480005f9f1cc20dd3538c794697c3
k2works <kakimomokuri@gmail.com>
2019-07-22 17:07:22 +0900

    feat: Studentモデルをスキャフォールディングする

---
+++ dev/20190722/k2works/app/ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+    }
+}
