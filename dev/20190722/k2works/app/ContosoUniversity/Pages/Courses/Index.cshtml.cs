bdcf40fd55776fba16778ce77a4b82768cffecdb
k2works <kakimomokuri@gmail.com>
2019-07-25 17:53:30 +0900

    feat: 部門名を表示する Course ページを作成する

---
+++ dev/20190722/k2works/app/ContosoUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Models.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CourseViewModel> CourseVM { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            CourseVM = await _context.Courses
+                .Select(p => new CourseViewModel
+                {
+                    CourseID = p.CourseID,
+                    Title = p.Title,
+                    Credits = p.Credits,
+                    DepartmentName = p.Department.Name
+                }).ToListAsync();
+        }
+    }
+}
