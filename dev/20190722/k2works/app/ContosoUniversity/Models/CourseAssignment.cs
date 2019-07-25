f9b363529f117b155430ae7bdb09c829f4cb7017
k2works <kakimomokuri@gmail.com>
2019-07-25 15:59:36 +0900

    feat: CourseAssignment エンティティ

--- dev/20190722/k2works/app/ContosoUniversity/Models/CourseAssignment.cs
+++ dev/20190722/k2works/app/ContosoUniversity/Models/CourseAssignment.cs
@@ -2,6 +2,10 @@ namespace ContosoUniversity.Models
 {
     public class CourseAssignment
     {
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+        public Instructor Instructor { get; set; }
+        public Course Course { get; set; }
         
     }
 }
--- dev/20190722/k2works/app/ContosoUniversity/Models/Enrollment.cs
+++ dev/20190722/k2works/app/ContosoUniversity/Models/Enrollment.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoUniversity.Models
 {
     public enum Grade
@@ -9,6 +11,7 @@ namespace ContosoUniversity.Models
        public int EnrollmentID { get; set; }
        public int CourseID { get; set; }
        public int StudentID { get; set; }
+       [DisplayFormat(NullDisplayText = "No grade")]
        public Grade? Grade { get; set; }
        
        public Course Course { get; set; }
