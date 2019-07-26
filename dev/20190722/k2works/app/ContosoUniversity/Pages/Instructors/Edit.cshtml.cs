8dc8ebac505a06997711dc124bde6b2c791f5337
k2works <kakimomokuri@gmail.com>
2019-07-26 18:36:07 +0900

    feat: Instructor ページを更新する

--- dev/20190722/k2works/app/ContosoUniversity/Pages/Instructors/Edit.cshtml.cs
+++ dev/20190722/k2works/app/ContosoUniversity/Pages/Instructors/Edit.cshtml.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Models;
 
@@ -12,9 +8,9 @@ namespace ContosoUniversity.Pages.Instructors
 {
     public class EditModel : PageModel
     {
-        private readonly ContosoUniversity.Models.SchoolContext _context;
+        private readonly SchoolContext _context;
 
-        public EditModel(ContosoUniversity.Models.SchoolContext context)
+        public EditModel(SchoolContext context)
         {
             _context = context;
         }
@@ -29,7 +25,10 @@ namespace ContosoUniversity.Pages.Instructors
                 return NotFound();
             }
 
-            Instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.ID == id);
+            Instructor = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
 
             if (Instructor == null)
             {
@@ -38,37 +37,32 @@ namespace ContosoUniversity.Pages.Instructors
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            _context.Attach(Instructor).State = EntityState.Modified;
+            var innstructorToUpdate = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .FirstOrDefaultAsync(s => s.ID == id);
 
-            try
+            if (await TryUpdateModelAsync<Instructor>(
+                innstructorToUpdate,
+                "Instructor",
+                i => i.FirstMidName, i => i.LastName,
+                i => i.HireDate, i => i.OfficeAssignment))
             {
+                if (string.IsNullOrWhiteSpace(
+                    innstructorToUpdate.OfficeAssignment?.Location))
+                {
+                    innstructorToUpdate.OfficeAssignment = null;
+                }
                 await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!InstructorExists(Instructor.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
             }
 
             return RedirectToPage("./Index");
-        }
-
-        private bool InstructorExists(int id)
-        {
-            return _context.Instructors.Any(e => e.ID == id);
         }
     }
 }
