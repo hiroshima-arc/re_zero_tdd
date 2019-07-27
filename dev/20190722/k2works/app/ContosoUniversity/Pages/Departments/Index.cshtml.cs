36b835dddcc652079f33eaab240350aa91fad0f3
k2works <kakimomokuri@gmail.com>
2019-07-27 11:21:09 +0900

    feat: 部署モデルのスキャフォールディング

---
+++ dev/20190722/k2works/app/ContosoUniversity/Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Models.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Department = await _context.Departments
+                .Include(d => d.Administrator).ToListAsync();
+        }
+    }
+}
