f18b4fdf998b77b093f5083aae9be842fbb27ad2
k2works <kakimomokuri@gmail.com>
2019-07-27 11:47:51 +0900

    feat: [削除] ページを更新する

--- dev/20190722/k2works/app/ContosoUniversity/Pages/Departments/Delete.cshtml.cs
+++ dev/20190722/k2works/app/ContosoUniversity/Pages/Departments/Delete.cshtml.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,49 +9,61 @@ namespace ContosoUniversity.Pages.Departments
 {
     public class DeleteModel : PageModel
     {
-        private readonly ContosoUniversity.Models.SchoolContext _context;
+        private readonly SchoolContext _context;
 
-        public DeleteModel(ContosoUniversity.Models.SchoolContext context)
+        public DeleteModel(SchoolContext context)
         {
             _context = context;
         }
 
-        [BindProperty]
-        public Department Department { get; set; }
+        [BindProperty] public Department Department { get; set; }
+        public string ConcurrencyErrorMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int? id, bool? concurrencyError)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             Department = await _context.Departments
-                .Include(d => d.Administrator).FirstOrDefaultAsync(m => m.DepartmentID == id);
+                .Include(d => d.Administrator)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.DepartmentID == id);
 
             if (Department == null)
             {
                 return NotFound();
             }
+
+            if (concurrencyError.GetValueOrDefault())
+            {
+                ConcurrencyErrorMessage = "The record you attempted to delete "
+                                          + "was modified by another user after you selected delete. "
+                                          + "The delete operation was canceled and the current values in the "
+                                          + "database have been displayed. If you still want to delete this "
+                                          + "record, click the Delete button again.";
+            }
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
+                if (await _context.Departments.AnyAsync(
+                    m => m.DepartmentID == id))
+                {
+                    // Department.rowVersion value is from when the entity
+                    // was fetched. If it doesn't match the DB, a
+                    // DbUpdateConcurrencyException exception is thrown.
+                    _context.Departments.Remove(Department);
+                    await _context.SaveChangesAsync();
+                }
+
+                return RedirectToPage("./Index");
             }
-
-            Department = await _context.Departments.FindAsync(id);
-
-            if (Department != null)
+            catch (DbUpdateConcurrencyException)
             {
-                _context.Departments.Remove(Department);
-                await _context.SaveChangesAsync();
+                return RedirectToPage("./Delete",
+                    new {concurrencyError = true, id = id});
             }
-
-            return RedirectToPage("./Index");
         }
     }
 }
