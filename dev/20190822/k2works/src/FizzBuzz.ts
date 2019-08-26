f9a797a7999377ee3f1cfe12cb7f604bf2b3dfa9
k2works <kakimomokuri@gmail.com>
2019-08-26 09:59:21 +0000

    refactor: シンボル定数によるマジックナンバーの置き換え

--- dev/20190822/k2works/src/FizzBuzz.ts
+++ dev/20190822/k2works/src/FizzBuzz.ts
@@ -1,16 +1,21 @@
 export class FizzBuzz {
+  public static readonly MAX_NUMBER: number = 100;
+  public static readonly FIZZ: string = "Fizz";
+  public static readonly BUZZ: string = "Buzz";
+  public static readonly FIZZ_BUZZ: string = "FizzBuzz";
+
   public static generate(num: number): string {
     const isFizz: boolean = num % 3 === 0;
     const isBuzz: boolean = num % 5 === 0;
 
     if (isFizz && isBuzz) {
-      return "FizzBuzz";
+      return this.FIZZ_BUZZ;
     }
     if (isFizz) {
-      return "Fizz";
+      return this.FIZZ;
     }
     if (isBuzz) {
-      return "Buzz";
+      return this.BUZZ;
     }
 
     return num.toString();
@@ -19,6 +24,6 @@ export class FizzBuzz {
     const range = (start: number, end: number) =>
       Array.from({ length: end - start + 1 }, (v, k) => k + start);
 
-    return range(1, 100).map(i => FizzBuzz.generate(i));
+    return range(1, this.MAX_NUMBER).map(i => FizzBuzz.generate(i));
   }
 }
