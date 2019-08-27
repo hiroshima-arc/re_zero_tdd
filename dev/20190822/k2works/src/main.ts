37c7dcd08d2a28d0340b9f5cd6e88e39bd4e99c1
k2works <kakimomokuri@gmail.com>
2019-08-27 08:07:18 +0000

    feat: FizzBuzzUI

--- dev/20190822/k2works/src/main.ts
+++ dev/20190822/k2works/src/main.ts
@@ -1,5 +1,50 @@
-import { greeting } from "./greeting";
+const table = `
+          <table>
+            <thead>
+              <tr>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+                <th>5</th>
+                <th>6</th>
+                <th>7</th>
+                <th>8</th>
+                <th>9</th>
+                <th>10</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                <td>10</td>
+              </tr>
+              <tr>
+                <td>11</td>
+                <td>12</td>
+                <td>13</td>
+                <td>14</td>
+                <td>15</td>
+                <td>16</td>
+                <td>17</td>
+                <td>18</td>
+                <td>19</td>
+                <td>20</td>
+              </tr>
+            </tbody>
+          </table>
+`;
 
-export function renderGreeting() {
-  document.querySelector("#app").innerHTML = greeting();
+export function renderTable() {
+  document.querySelector("#app").innerHTML = table;
 }
+
+renderTable();
