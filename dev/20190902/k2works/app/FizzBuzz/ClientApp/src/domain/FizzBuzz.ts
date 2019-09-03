befb7ae73103810e53469aff7f42920d80d1c120
k2works <kakimomokuri@gmail.com>
2019-09-03 14:24:37 +0900

    refactor: シンボル定数いよるマジックナンバーの置き換え

--- dev/20190902/k2works/app/FizzBuzz/ClientApp/src/domain/FizzBuzz.ts
+++ dev/20190902/k2works/app/FizzBuzz/ClientApp/src/domain/FizzBuzz.ts
@@ -1,22 +1,27 @@
 export class FizzBuzz {
+  public static readonly MAX_NUMBER: number = 100;
+  public static readonly FIZZ: string = "Fizz";
+  public static readonly BUZZ: string = "Buzz";
+  public static readonly FIZZ_BUZZ: string = FizzBuzz.FIZZ + FizzBuzz.BUZZ;
+
   public static readonly range = (start: number, end: number) =>
     Array.from({ length: end - start + 1 }, (v, k) => k + start);
 
   public static generateList(): string[] {
-    return FizzBuzz.range(1, 100).map(i => FizzBuzz.generate(i));
+    return FizzBuzz.range(1, this.MAX_NUMBER).map(i => FizzBuzz.generate(i));
   }
 
   public static generate(input: number) {
     const isFizz: boolean = input % 3 === 0;
     const isBuzz: boolean = input % 5 === 0;
     if (isFizz && isBuzz) {
-      return "FizzBuzz";
+      return this.FIZZ_BUZZ;
     }
     if (isFizz) {
-      return "Fizz";
+      return this.FIZZ;
     }
     if (isBuzz) {
-      return "Buzz";
+      return this.BUZZ;
     }
     return input.toString();
   }
