a41dd2435de65083b99625aec408cc510605d982
k2works <kakimomokuri@gmail.com>
2019-09-03 13:43:32 +0900

    feat: FizzBuzzUI

--- dev/20190902/k2works/app/FizzBuzz/ClientApp/src/components/Home.tsx
+++ dev/20190902/k2works/app/FizzBuzz/ClientApp/src/components/Home.tsx
@@ -1,56 +1,44 @@
 import React, { Component } from "react";
+import { FizzBuzz } from "../domain/FizzBuzz";
 
 export class Home extends Component {
   public static displayName = Home.name;
+
+  private table = (() => {
+    const header = (() => {
+      let element = "<thead><tr>";
+      for (let i = 1; i <= 10; i++) {
+        element += `<th>${i}</th>`;
+      }
+      element += "</tr></thead>";
+      return element;
+    })();
+
+    const body = (() => {
+      let element = "<tbody><tr>";
+      for (let i = 1; i <= 100; i++) {
+        element += `<td>${FizzBuzz.generate(i)}</td>`;
+        if (i % 10 === 0) {
+          element += "</tr>";
+        }
+      }
+      element += "</tbody>";
+      return element;
+    })();
+
+    return `
+          <table class="table">
+          ${header}
+          ${body}
+          </table>
+`;
+  })();
 
   public render() {
     return (
       <div>
         <h2>FizzBuzz</h2>
-        <div id="app">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-                <th>4</th>
-                <th>5</th>
-                <th>6</th>
-                <th>7</th>
-                <th>8</th>
-                <th>9</th>
-                <th>10</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-                <td>10</td>
-              </tr>
-              <tr>
-                <td>11</td>
-                <td>12</td>
-                <td>13</td>
-                <td>14</td>
-                <td>15</td>
-                <td>16</td>
-                <td>17</td>
-                <td>18</td>
-                <td>19</td>
-                <td>20</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <div id="app" dangerouslySetInnerHTML={{ __html: this.table }} />
       </div>
     );
   }
