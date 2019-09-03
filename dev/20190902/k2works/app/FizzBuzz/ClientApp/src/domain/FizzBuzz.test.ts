d26fa2667d2e1ff837ef77596c92064af467a4c5
k2works <kakimomokuri@gmail.com>
2019-09-03 12:41:00 +0900

    feat: 1から100までの数をプリントする

--- dev/20190902/k2works/app/FizzBuzz/ClientApp/src/domain/FizzBuzz.test.ts
+++ dev/20190902/k2works/app/FizzBuzz/ClientApp/src/domain/FizzBuzz.test.ts
@@ -1,8 +1,7 @@
-import {FizzBuzz} from "./FizzBuzz";
+import { FizzBuzz } from "./FizzBuzz";
 
 test("1から100までをプリントする", () => {
   const list: any[] = FizzBuzz.print1To100();
   expect(list[0]).toBe(1);
   expect(list[99]).toBe(100);
 });
-    
